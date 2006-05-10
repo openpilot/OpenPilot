aa91b42fd32002a3528a70b0b3eaf2013b339e61
tlemaire <tlemaire>
2006-05-10 13:00:18 +0000

    add a LAPACK_ERROR Exception, to report errors which may occur during lapack routines execution, the INFO lapack diagnostic parameter is included in the exception class.
    git-svn-id: svn+ssh://svn.laas.fr/svn/jafar/jafarModules/trunk/jmath@1673 d3dd4ecc-a6df-0310-ac42-d4bc92953605

--- include/jmath/jmathException.hpp
+++ include/jmath/jmathException.hpp
@@ -22,7 +22,7 @@ namespace jafar {
        * Jmath.
        */
       enum ExceptionId {
-        //        MY_ERROR /**< my error */
+	LAPACK_ERROR /**< Error reported by a lapack routine */
       };
 
       /** Constructor. You should not use this constructor directly,
@@ -35,8 +35,8 @@ namespace jafar {
        * @param line_ where the exception was thrown
        */
       JmathException(ExceptionId id_, 
-                            const std::string& message_, 
-                            const std::string& file_, int line_) throw();
+		     const std::string& message_, 
+		     const std::string& file_, int line_) throw();
 
       virtual ~JmathException() throw();
 
@@ -49,6 +49,48 @@ namespace jafar {
       static std::string exceptionIdToString(ExceptionId id_) throw();
       
     }; // class JmathException
+
+    /** This exception is thrown when a lapack routine fails.
+     * Quote from lapack documentation:
+     *
+     * Error Handling and the Diagnostic Argument INFO 
+     *
+     *All documented routines have a diagnostic argument INFO that
+     *indicates the success or failure of the computation, as follows:
+     *
+     * - INFO = 0: successful termination 
+     * - INFO < 0: illegal value of one *or more arguments -- no
+     * computation performed 
+     * - INFO > 0: failure in the course of computation 
+     *
+     * All driver and auxiliary routines check that input arguments
+     * such as N or LDA or option arguments of type character have
+     * permitted values. If an illegal value of the ith argument is
+     * detected, the routine sets INFO = -i, and then calls an
+     * error-handling routine XERBLA.  The standard version of XERBLA
+     * issues an error message and halts execution, so that no LAPACK
+     * routine would ever return to the calling program with INFO <
+     * 0. However, this might occur if a non-standard version of
+     * XERBLA is used.
+     *
+     * \ingroup jmath
+     */
+    class LapackException : public JmathException {
+
+    public:
+
+      int info;
+
+      LapackException(int info_,
+		      const std::string& message_, 
+		      const std::string& file_, int line_) throw() :
+	JmathException(LAPACK_ERROR, message_, file_, line_),
+	info(info_)
+      {}
+
+      virtual ~LapackException() throw() {}
+
+    };
 
   } // namespace jmath
 } // namespace jafar

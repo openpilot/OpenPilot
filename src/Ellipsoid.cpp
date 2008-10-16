660827dda440ab06c6437e0985910b899390c288
cberger <cberger>
2008-10-16 13:35:46 +0000

    use a size 2 vector for the eigen values
    git-svn-id: svn+ssh://svn.laas.fr/svn/jafar/jafarModules/trunk/qdisplay@3244 d3dd4ecc-a6df-0310-ac42-d4bc92953605

--- src/Ellipsoid.cpp
+++ src/Ellipsoid.cpp
@@ -13,7 +13,7 @@ using namespace jafar::qdisplay;
 Ellipsoid::Ellipsoid( const jblas::vec2& _x, const jblas::sym_mat22& _xCov ) : Shape( Shape::ShapeEllipse, 0.0, 0.0, 1.0, 1.0 )
 {
   namespace lapack = boost::numeric::bindings::lapack;
-  jblas::vec lambda(3);
+  jblas::vec2 lambda;
   ublas::matrix<double, ublas::column_major> A(ublas::project(_xCov, ublas::range(0,2), ublas::range(0,2))); 
   int ierr = lapack::syev( 'V', 'U', A, lambda, lapack::optimal_workspace() );
   JFR_POSTCOND(ierr==0,
@@ -21,8 +21,9 @@ Ellipsoid::Ellipsoid( const jblas::vec2& _x, const jblas::sym_mat22& _xCov ) : S
   if (!ierr==0) {
     JFR_WARNING("Ellipsoid::Ellipsoid: error in lapack::syev() function, ierr=" << ierr);
   } else {
-    setBoundingBox( _x(0), _x(1), lambda(0), lambda( 1 ) );
-    
+    setBoundingBox( _x(0), _x(1), 1, 1 );
+//     setBoundingBox( _x(0), _x(1), lambda(0), lambda( 1 ) );
+    JFR_DEBUG( lambda );
     setTransform( QTransform( A(0,0), A(0,1), A(1,0), A(1, 1), 0, 0 ) );
     
   }

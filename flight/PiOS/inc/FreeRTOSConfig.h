08fc0ac743719e6fd8daa4f65cc410a12459e38f
dankers <dankers>
2009-12-23 10:07:05 +0000

    Add notes section. To be completed as we get more advanced.
    git-svn-id: svn://svn.openpilot.org/OpenPilot/trunk@110 ebee16cc-31ac-478f-84a7-5cbb03baadba

--- flight/PiOS/inc/FreeRTOSConfig.h
+++ flight/PiOS/inc/FreeRTOSConfig.h
@@ -14,6 +14,7 @@
  * See http://www.freertos.org/a00110.html.
  *----------------------------------------------------------*/
 
+/* Notes: We use 5 task priorities */  
 
 #define configUSE_PREEMPTION		1
 #define configUSE_IDLE_HOOK		1

acdf7bd95904da38bfb209bf15fc36a9e89dbcc5
jsola <jsola>
2010-03-11 17:10:15 +0000

    Added matlab.cpp, forgot from last commit.
    git-svn-id: svn+ssh://svn.laas.fr/svn/jafar/jafarModules/trunk/jmath@4370 d3dd4ecc-a6df-0310-ac42-d4bc92953605

---
+++ src/matlab.cpp
@@ -0,0 +1,24 @@
+/**
+ * matlab.cpp
+ *
+ *  Created on: 11/03/2010
+ *      Author: jsola
+ *
+ *  \file matlab.cpp
+ *
+ *  ## Add a description here ##
+ *
+ * \ingroup jmath
+ */
+
+#include "jmath/matlab.hpp"
+
+namespace jafar{
+	namespace jmath{
+		bool MATLAB::fullPrec = true;
+
+	}
+}
+
+
+

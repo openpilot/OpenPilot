6ef67db776b7d40d1d54ac92ce7d609d9efb02fa
Joan Solà <jsola@picadestats.local>
2010-03-14 10:25:51 +0100

    Added this file, forgot from last commit.

---
+++ src/observationPinHoleAnchoredHomogeneous.cpp
@@ -0,0 +1,28 @@
+/**
+ * observationPinHoleAnchoredHomogeneous.cpp
+ *
+ *  Created on: 14/03/2010
+ *      Author: jsola
+ *
+ *  \file observationPinHoleAnchoredHomogeneous.cpp
+ *
+ *  ## Add a description here ##
+ *
+ * \ingroup rtslam
+ */
+
+#include "rtslam/observationPinHoleAnchoredHomogeneous.hpp"
+
+namespace jafar {
+	namespace rtslam {
+
+		using namespace std;
+		using namespace jblas;
+
+		ObservationPinHoleAnchoredHomogeneousPoint::ObservationPinHoleAnchoredHomogeneousPoint() :
+			ObservationPinHolePoint(){
+		}
+
+	}
+}
+

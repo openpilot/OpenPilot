19db0ee149be0ac222e35a3610af542093008bef
Joan Solà <jsola@picadestats.local>
2010-03-18 23:01:39 +0100

    added rtslam.hpp which is supposed to supervise all links between objects in the rtslam project. added objectAbstract.cpp, forgot from last commit.

---
+++ src/objectAbstract.cpp
@@ -0,0 +1,37 @@
+/**
+ * \file objectAbstract.cpp
+ *
+ *  Created on: 18/03/2010
+ *     \author: jsola@laas.fr
+ *
+ *
+ *  ## Add a description here ##
+ *
+ * \ingroup rtslam
+ */
+
+#include "rtslam/objectAbstract.hpp"
+
+namespace jafar {
+	namespace rtslam {
+		using namespace std;
+
+
+		/*
+		 * Operator << for class ObjectAbstract.
+		 * It shows different information of the object.
+		 */
+		std::ostream& operator <<(std::ostream & s, jafar::rtslam::ObjectAbstract & obj) {
+			s << obj.categoryName() << " " << obj.id() << ": ";
+			if (obj.name().size() > 0)
+				s << obj.name() << ", ";
+			s << "of type " << obj.type();
+			return s;
+		}
+
+		ObjectAbstract::ObjectAbstract() :
+			id_(0), categoryName_("OBJECT") {
+		}
+
+	}
+}

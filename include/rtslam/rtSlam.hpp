b792e47648c48a29743972b3a3cf6c259c31b2e0
Joan Solà <jsola@picadestats.laas.fr>
2010-03-24 12:34:00 +0100

    some typedefs added and commented out, just for reference.

--- include/rtslam/rtSlam.hpp
+++ include/rtslam/rtSlam.hpp
@@ -10,8 +10,6 @@
  * \ingroup rtslam
  */
 
-
-
 #ifndef RTSLAM_HPP_
 #define RTSLAM_HPP_
 
@@ -20,9 +18,9 @@
 #include <map>
 #include <list>
 
-
 namespace jafar {
 	namespace rtslam {
+
 
 		// forward declarations
 		class MapAbstract;
@@ -31,12 +29,21 @@ namespace jafar {
 		class LandmarkAbstract;
 		class ObservationAbstract;
 
+
+		//		// Regular Pointers:
+		//		typedef MapAbstract* map_ptr_t;
+		//		typedef RobotAbstract* robot_ptr_t;
+		//		typedef SensorAbstract* sensor_ptr_t;
+		//		typedef LandmarkAbstract* landmark_ptr_t;
+		//		typedef ObservationAbstract* observation_ptr_t;
+
 		// Pointers with boost::shared_ptr:
 		typedef boost::shared_ptr<MapAbstract> map_ptr_t;
 		typedef boost::shared_ptr<RobotAbstract> robot_ptr_t;
 		typedef boost::shared_ptr<SensorAbstract> sensor_ptr_t;
 		typedef boost::shared_ptr<LandmarkAbstract> landmark_ptr_t;
 		typedef boost::shared_ptr<ObservationAbstract> observation_ptr_t;
+
 
 		// Pointers with boost::weak_ptr:
 		typedef boost::weak_ptr<MapAbstract> map_wkptr_t;
@@ -45,12 +52,13 @@ namespace jafar {
 		typedef boost::weak_ptr<LandmarkAbstract> landmark_wkptr_t;
 		typedef boost::weak_ptr<ObservationAbstract> observation_wkptr_t;
 
-//		// Pointers: upwards
-//		typedef map_ptr_t map_ptr_t;
-//		typedef robot_ptr_t robot_ptr_t;
-//		typedef sensor_ptr_t sensor_ptr_t;
-//		typedef landmark_ptr_t landmark_ptr_t;
-//		typedef observation_ptr_t observation_ptr_t;
+
+		//		// Pointers: upwards
+		//		typedef map_ptr_t map_ptr_t;
+		//		typedef robot_ptr_t robot_ptr_t;
+		//		typedef sensor_ptr_t sensor_ptr_t;
+		//		typedef landmark_ptr_t landmark_ptr_t;
+		//		typedef observation_ptr_t observation_ptr_t;
 
 		// Pointer sets: downwards
 		typedef std::map<size_t, map_ptr_t> map_ptr_set_t;

c8ebd06b5001f6b472103f9e09b29c49b3e43b4d
dankers <dankers>
2010-03-24 08:42:43 +0000

    Remove build directory, it should never have been checked in and was just me being lazy.
    git-svn-id: svn://svn.openpilot.org/OpenPilot/trunk@377 ebee16cc-31ac-478f-84a7-5cbb03baadba

--- ground/Build/run.bat
+++
@@ -1,8 +0,0 @@
-set QTDIR=C:\Qt\2010.01\qt
-set PATH=C:\Qt\2010.01\qt\bin
-set PATH=%PATH%;C:\Qt\2010.01\bin;C:\Qt\2010.01\mingw\bin
-set PATH=%PATH%;%SystemRoot%\System32
-set QMAKESPEC=win32-g++
-
-
-bin\openpilotgcs.exe

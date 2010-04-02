4e46c707fc42aeeda4bd10266f1843285746dd1a
Joan Solà <jsola@picadestats.laas.fr>
2010-04-02 15:35:30 +0200

    Added automatic tests JFR_CHECK_...(). Everything works.

--- test_suite/test_ahp.cpp
+++ test_suite/test_ahp.cpp
@@ -15,6 +15,7 @@
 
 // jafar debug include
 #include "kernel/jafarDebug.hpp"
+#include "kernel/jafarTestMacro.hpp"
 
 #include <iostream>
 #include "jmath/matlab.hpp"
@@ -41,8 +42,10 @@ void test_ahp01(void) {
 	mat V_ahp(3, 7);
 
 	cout << "\n% INIT \n%============" << endl;
-	randVector(ahp);
-	randVector(v);
+	for (size_t i = 0; i < 7; i++)
+		ahp(i) = i + 5;
+	for (size_t i = 0; i < 3; i++)
+		v(i) = i + 2;
 	subrange(s, 0, 3) = v;
 	subrange(s, 3, 7) = quaternion::e2q(v);
 	r = 1;
@@ -58,6 +61,12 @@ void test_ahp01(void) {
 	ahp = fromBearingOnlyFrame(s, v, r);
 	cout << "ahp1 = " << (MATLAB) ahp << endl;
 	fromBearingOnlyFrame(s, v, r, ahp, AHP_s, AHP_v, AHP_r);
+	JFR_CHECK_EQUAL(ahp(3), -2.5013342);
+	JFR_CHECK_EQUAL(AHP_s(3,5), 3.5702252);
+	JFR_CHECK_EQUAL(AHP_s(5,5), 0.676264);
+	JFR_CHECK_EQUAL(AHP_v(3,2), -0.649772332351480);
+	JFR_CHECK_EQUAL(AHP_v(6,1), 0.557086014531156);
+	JFR_CHECK_EQUAL(AHP_r(6,0), 5.385164807134504);
 	cout << "ahp2 = " << (MATLAB) ahp << endl;
 	cout << "AHP_s = " << (MATLAB) AHP_s << endl;
 	cout << "AHP_v = " << (MATLAB) AHP_v << endl;
@@ -74,6 +83,9 @@ void test_ahp01(void) {
 	v = toBearingOnlyFrame(s, ahp);
 	cout << "v1 = " << (MATLAB) v << endl;
 	toBearingOnlyFrame(s, ahp, v, V_s, V_ahp);
+	JFR_CHECK_EQUAL(v(2), 4.0);
+	JFR_CHECK_EQUAL(V_s(0,1), -4.034721000604516);
+	JFR_CHECK_EQUAL(V_ahp(2,1), 3.440047966271193);
 	cout << "v2 = " << (MATLAB) v << endl;
 	cout << "V_s = " << (MATLAB) V_s << endl;
 	cout << "V_ahp = " << (MATLAB) V_ahp << endl;
@@ -82,9 +94,11 @@ void test_ahp01(void) {
 	cout << "V_s_err = norm(V_s - V_s_mat)" << endl;
 	cout << "V_ahp_err = norm(V_ahp - V_ahp_mat)" << endl;
 
-	cout << "\n% FWD AND BCKWD JACOBIANS\n%===================" << endl;
+	cout << "\n% PRODUCT OF FWD AND BCKWD JACOBIANS\n%===================" << endl;
 	mat M3(3, 3), M7(7, 7);
 	M3 = ublas::prod(V_ahp, AHP_v);
+	JFR_CHECK_EQUAL(M3(0,1), 0.0);
+	JFR_CHECK_EQUAL(M3(1,1), 1.0); // M3 = V_ahp * AHP_v must be a identity matrix
 	M7 = ublas::prod(AHP_v, V_ahp);
 	cout << "V_v = " << (MATLAB) M3 << endl;
 	cout << "AHP_ahp = " << (MATLAB) M7 << endl;

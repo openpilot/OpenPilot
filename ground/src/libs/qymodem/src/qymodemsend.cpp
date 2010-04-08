1ec4775be64dae97bac1f2195321df7262273fd4
zedamota <zedamota>
2010-04-08 20:32:52 +0000

    Added license info
    git-svn-id: svn://svn.openpilot.org/OpenPilot/trunk@443 ebee16cc-31ac-478f-84a7-5cbb03baadba

--- ground/src/libs/qymodem/src/qymodemsend.cpp
+++ ground/src/libs/qymodem/src/qymodemsend.cpp
@@ -1,3 +1,19 @@
+/*
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ */
+
 #include "qymodemsend.h"
 
 
@@ -82,9 +98,6 @@ public:
     int In(char* data, size_t size, int * percent)
     {
         *percent = TotalSize ? ((uint64_t)TransferredSize*(uint64_t)100)/(uint64_t)TotalSize : 0;
-
-        //printf("%8d bytes of %d - %3d%%\r",TransferredSize, TotalSize, percent);
-
         fflush(stdout);
         size=fread(data,sizeof(uint8_t),size,File);
         if(size)

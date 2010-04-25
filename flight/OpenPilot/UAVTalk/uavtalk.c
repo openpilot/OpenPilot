613b17f97066f4d07bf733737f9690fd47aadddd
vassilis <vassilis>
2010-04-25 21:20:13 +0000

    OP-4 Flight/Telemetry Implement CRC in telemetry protocol
    git-svn-id: svn://svn.openpilot.org/OpenPilot/trunk@549 ebee16cc-31ac-478f-84a7-5cbb03baadba

--- flight/OpenPilot/UAVTalk/uavtalk.c
+++ flight/OpenPilot/UAVTalk/uavtalk.c
@@ -36,9 +36,29 @@
 #define TYPE_ACK (TYPE_VER | 0x03)
 
 #define HEADER_LENGTH 7 // type (1), object ID (4), instance ID (2, not used in single objects)
-#define CHECKSUM_LENGTH 2
+#define CHECKSUM_LENGTH 1
 #define MAX_PAYLOAD_LENGTH 256
 #define MAX_PACKET_LENGTH (HEADER_LENGTH+MAX_PAYLOAD_LENGTH+CHECKSUM_LENGTH)
+
+// CRC lookup table
+static const uint8_t crc_table[256] = {
+    0x00, 0x07, 0x0e, 0x09, 0x1c, 0x1b, 0x12, 0x15, 0x38, 0x3f, 0x36, 0x31, 0x24, 0x23, 0x2a, 0x2d,
+    0x70, 0x77, 0x7e, 0x79, 0x6c, 0x6b, 0x62, 0x65, 0x48, 0x4f, 0x46, 0x41, 0x54, 0x53, 0x5a, 0x5d,
+    0xe0, 0xe7, 0xee, 0xe9, 0xfc, 0xfb, 0xf2, 0xf5, 0xd8, 0xdf, 0xd6, 0xd1, 0xc4, 0xc3, 0xca, 0xcd,
+    0x90, 0x97, 0x9e, 0x99, 0x8c, 0x8b, 0x82, 0x85, 0xa8, 0xaf, 0xa6, 0xa1, 0xb4, 0xb3, 0xba, 0xbd,
+    0xc7, 0xc0, 0xc9, 0xce, 0xdb, 0xdc, 0xd5, 0xd2, 0xff, 0xf8, 0xf1, 0xf6, 0xe3, 0xe4, 0xed, 0xea,
+    0xb7, 0xb0, 0xb9, 0xbe, 0xab, 0xac, 0xa5, 0xa2, 0x8f, 0x88, 0x81, 0x86, 0x93, 0x94, 0x9d, 0x9a,
+    0x27, 0x20, 0x29, 0x2e, 0x3b, 0x3c, 0x35, 0x32, 0x1f, 0x18, 0x11, 0x16, 0x03, 0x04, 0x0d, 0x0a,
+    0x57, 0x50, 0x59, 0x5e, 0x4b, 0x4c, 0x45, 0x42, 0x6f, 0x68, 0x61, 0x66, 0x73, 0x74, 0x7d, 0x7a,
+    0x89, 0x8e, 0x87, 0x80, 0x95, 0x92, 0x9b, 0x9c, 0xb1, 0xb6, 0xbf, 0xb8, 0xad, 0xaa, 0xa3, 0xa4,
+    0xf9, 0xfe, 0xf7, 0xf0, 0xe5, 0xe2, 0xeb, 0xec, 0xc1, 0xc6, 0xcf, 0xc8, 0xdd, 0xda, 0xd3, 0xd4,
+    0x69, 0x6e, 0x67, 0x60, 0x75, 0x72, 0x7b, 0x7c, 0x51, 0x56, 0x5f, 0x58, 0x4d, 0x4a, 0x43, 0x44,
+    0x19, 0x1e, 0x17, 0x10, 0x05, 0x02, 0x0b, 0x0c, 0x21, 0x26, 0x2f, 0x28, 0x3d, 0x3a, 0x33, 0x34,
+    0x4e, 0x49, 0x40, 0x47, 0x52, 0x55, 0x5c, 0x5b, 0x76, 0x71, 0x78, 0x7f, 0x6a, 0x6d, 0x64, 0x63,
+    0x3e, 0x39, 0x30, 0x37, 0x22, 0x25, 0x2c, 0x2b, 0x06, 0x01, 0x08, 0x0f, 0x1a, 0x1d, 0x14, 0x13,
+    0xae, 0xa9, 0xa0, 0xa7, 0xb2, 0xb5, 0xbc, 0xbb, 0x96, 0x91, 0x98, 0x9f, 0x8a, 0x8d, 0x84, 0x83,
+    0xde, 0xd9, 0xd0, 0xd7, 0xc2, 0xc5, 0xcc, 0xcb, 0xe6, 0xe1, 0xe8, 0xef, 0xfa, 0xfd, 0xf4, 0xf3
+};
 
 // Private types
 typedef enum {STATE_SYNC, STATE_OBJID, STATE_INSTID, STATE_DATA, STATE_CS} RxState;
@@ -55,7 +75,7 @@ static uint8_t txBuffer[MAX_PACKET_LENGTH];
 static UAVTalkStats stats;
 
 // Private functions
-static uint16_t updateChecksum(uint16_t cs, uint8_t* data, int32_t length);
+static uint8_t updateCRC(uint8_t crc, const uint8_t* data, int32_t length);
 static int32_t objectTransaction(uint32_t objectId, uint16_t instId, uint8_t type, int32_t timeout);
 static int32_t sendObject(UAVObjHandle obj, uint16_t instId, uint8_t type);
 static int32_t sendSingleObject(UAVObjHandle obj, uint16_t instId, uint8_t type);
@@ -218,7 +238,7 @@ int32_t UAVTalkProcessInputStream(uint8_t rxbyte)
     static uint32_t objId;
     static uint16_t instId;
     static uint32_t length;
-    static uint16_t cs, csRx;
+    static uint8_t cs, csRx;
     static int32_t rxCount;
     static RxState state = STATE_SYNC;
 
@@ -228,7 +248,7 @@ int32_t UAVTalkProcessInputStream(uint8_t rxbyte)
     case STATE_SYNC:
         if ((rxbyte & TYPE_MASK) == TYPE_VER )
         {
-            cs = rxbyte;
+            cs = updateCRC(0, &rxbyte, 1);
             type = rxbyte;
             state = STATE_OBJID;
             rxCount = 0;
@@ -249,7 +269,7 @@ int32_t UAVTalkProcessInputStream(uint8_t rxbyte)
             else
             {
             	// Update checksum
-                cs = updateChecksum(cs, tmpBuffer, 4);
+                cs = updateCRC(cs, tmpBuffer, 4);
                 // Determine data length
                 if (type == TYPE_OBJ_REQ || type == TYPE_ACK)
                 {
@@ -296,7 +316,7 @@ int32_t UAVTalkProcessInputStream(uint8_t rxbyte)
         if (rxCount == 2)
         {
         	instId = (tmpBuffer[1] << 8) | (tmpBuffer[0]);
-        	cs = updateChecksum(cs, tmpBuffer, 2);
+        	cs = updateCRC(cs, tmpBuffer, 2);
         	rxCount = 0;
         	// If there is a payload get it, otherwise receive checksum
         	if (length > 0)
@@ -313,30 +333,26 @@ int32_t UAVTalkProcessInputStream(uint8_t rxbyte)
         rxBuffer[rxCount++] = rxbyte;
         if (rxCount == length)
         {
-            cs = updateChecksum(cs, rxBuffer, length);
+            cs = updateCRC(cs, rxBuffer, length);
             state = STATE_CS;
             rxCount = 0;
         }
         break;
     case STATE_CS:
-        tmpBuffer[rxCount++] = rxbyte;
-        if (rxCount == 2)
+    	csRx = rxbyte;
+        if (csRx == cs)
         {
-            csRx = (tmpBuffer[1] << 8) | (tmpBuffer[0]);
-            if (csRx == cs)
-            {
-                xSemaphoreTakeRecursive(lock, portMAX_DELAY);
-                receiveObject(type, obj, instId, rxBuffer, length);
-                ++stats.rxObjects;
-                stats.rxObjectBytes += length;
-                xSemaphoreGiveRecursive(lock);
-            }
-            else
-            {
-            	++stats.rxErrors;
-            }
-            state = STATE_SYNC;
+        	xSemaphoreTakeRecursive(lock, portMAX_DELAY);
+            receiveObject(type, obj, instId, rxBuffer, length);
+            ++stats.rxObjects;
+            stats.rxObjectBytes += length;
+            xSemaphoreGiveRecursive(lock);
         }
+        else
+        {
+        	++stats.rxErrors;
+        }
+        state = STATE_SYNC;
         break;
     default:
         state = STATE_SYNC;
@@ -502,7 +518,6 @@ static int32_t sendSingleObject(UAVObjHandle obj, uint16_t instId, uint8_t type)
 {
     int32_t length;
     int32_t dataOffset;
-    uint16_t cs = 0;
     uint32_t objId;
 
     // Setup type and object id fields
@@ -551,10 +566,7 @@ static int32_t sendSingleObject(UAVObjHandle obj, uint16_t instId, uint8_t type)
     }
 
     // Calculate checksum
-    cs = 0;
-    cs = updateChecksum(cs, txBuffer, dataOffset+length);
-    txBuffer[dataOffset+length] = (uint8_t)(cs & 0xFF);
-    txBuffer[dataOffset+length+1] = (uint8_t)((cs >> 8) & 0xFF);
+    txBuffer[dataOffset+length] = updateCRC(0, txBuffer, dataOffset+length);
 
     // Send buffer
     if (outStream!=NULL) (*outStream)(txBuffer, dataOffset+length+CHECKSUM_LENGTH);
@@ -569,24 +581,34 @@ static int32_t sendSingleObject(UAVObjHandle obj, uint16_t instId, uint8_t type)
 }
 
 /**
- * Update checksum.
- * TODO: Replace with CRC-16
- * \param[in] data Data buffer to update checksum on
- * \param[in] length Length of buffer
- * \return Updated checksum
+ * Update the crc value with new data.
+ *
+ * Generated by pycrc v0.7.5, http://www.tty1.net/pycrc/
+ * using the configuration:
+ *    Width        = 8
+ *    Poly         = 0x07
+ *    XorIn        = 0x00
+ *    ReflectIn    = False
+ *    XorOut       = 0x00
+ *    ReflectOut   = False
+ *    Algorithm    = table-driven
+ *
+ * \param crc      The current crc value.
+ * \param data     Pointer to a buffer of \a data_len bytes.
+ * \param length   Number of bytes in the \a data buffer.
+ * \return         The updated crc value.
  */
-static uint16_t updateChecksum(uint16_t cs, uint8_t* data, int32_t length)
+static uint8_t updateCRC(uint8_t crc, const uint8_t* data, int32_t length)
 {
-    int32_t n;
-    for (n = 0; n < length; ++n)
-    {
-        cs += (uint16_t)data[n];
+    uint32_t tbl_idx;
+
+    while (length--) {
+        tbl_idx = ((crc >> 0) ^ *data) & 0xff;
+        crc = (crc_table[tbl_idx] ^ (crc << 8)) & 0xff;
+        data++;
     }
-    return cs;
+    return crc;
 }
-
-
-
 
 
 

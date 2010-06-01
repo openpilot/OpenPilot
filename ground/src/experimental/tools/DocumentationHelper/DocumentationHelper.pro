9de32fa040cbf53c4bcf7d9d6f94a36be23f85e9
zedamota <zedamota>
2010-06-01 22:28:45 +0000

    GCS/Tools DocumentationHelper - Just a small app to help with my laziness Just choose a folder and the app will fill the license header in .h and .cpp files with the proper filename and user selectable DefGroup. Also inside the same folder it can create a namespace with user selectable name. Additionally it can create #if #endif blocks around qDebug's ex #ifdef DEBUG_name_of_file_uppercase. This was made for my needs, use it at your own risk.
    git-svn-id: svn://svn.openpilot.org/OpenPilot/trunk@701 ebee16cc-31ac-478f-84a7-5cbb03baadba

---
+++ ground/src/experimental/tools/DocumentationHelper/DocumentationHelper.pro
@@ -0,0 +1,16 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2010-05-30T17:21:05
+#
+#-------------------------------------------------
+
+TARGET = DocumentationHelper
+TEMPLATE = app
+
+
+SOURCES += main.cpp\
+        mainwindow.cpp
+
+HEADERS  += mainwindow.h
+
+FORMS    += mainwindow.ui
---
+++ ground/src/experimental/tools/DocumentationHelper/mainwindow.h
@@ -0,0 +1,60 @@
+/**
+******************************************************************************
+*
+* @file       mainwindow.h
+* @author     The OpenPilot Team, http://www.openpilot.org Copyright (C) 2010.
+*             Parts by Nokia Corporation (qt-info@nokia.com) Copyright (C) 2009.
+* @brief      
+* @see        The GNU Public License (GPL) Version 3
+* @defgroup   DocumentationHelper
+* @{
+* 
+*****************************************************************************/
+/* 
+* This program is free software; you can redistribute it and/or modify 
+* it under the terms of the GNU General Public License as published by 
+* the Free Software Foundation; either version 3 of the License, or 
+* (at your option) any later version.
+* 
+* This program is distributed in the hope that it will be useful, but 
+* WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY 
+* or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License 
+* for more details.
+* 
+* You should have received a copy of the GNU General Public License along 
+* with this program; if not, write to the Free Software Foundation, Inc., 
+* 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+*/
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QDir>
+#include <QStringList>
+#include <QDebug>
+#include <QMessageBox>
+
+namespace Ui {
+    class MainWindow;
+}
+
+class MainWindow : public QMainWindow {
+    Q_OBJECT
+public:
+    MainWindow(QWidget *parent = 0);
+    ~MainWindow();
+
+protected:
+    void changeEvent(QEvent *e);
+
+private:
+    Ui::MainWindow *ui;
+    QString wpath;
+    QStringList license;
+
+private slots:
+    void on_goBT_clicked();
+    void on_cpathBT_clicked();
+};
+
+#endif // MAINWINDOW_H

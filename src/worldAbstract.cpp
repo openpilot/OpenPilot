96fa38768640deac541b44cfc93e517576c50166
Cyril Roussillon <cyril.roussillon@laas.fr>
2010-06-10 14:01:32 +0200

    add missing file worldAbstract.cpp

---
+++ src/worldAbstract.cpp
@@ -0,0 +1,39 @@
+/**
+ * worldAbstract.cpp
+ *
+ * \date 08/06/2010
+ * \author croussil@laas.fr
+ *
+ *  \file worldAbstract.cpp
+ *
+ *  ## Add a description here ##
+ *
+ * \ingroup rtslam
+ */
+
+#include "rtslam/worldAbstract.hpp"
+#include "rtslam/display.hpp"
+
+namespace jafar {
+	namespace rtslam {
+
+		void WorldAbstract::addDisplayViewer(display::ViewerAbstract *viewer, unsigned id)
+		{
+			if (display_viewers.size() <= id)
+			{
+				int oldsize = display_viewers.size();
+				display_viewers.resize(id+1);
+				for(unsigned int i = oldsize; i < id; ++i)
+					display_viewers[i] = NULL;
+			}
+			display_viewers[id] = viewer;
+		}
+		
+		display::ViewerAbstract* WorldAbstract::getDisplayViewer(unsigned id)
+		{
+			if (display_viewers.size() <= id) return NULL;
+			return display_viewers[id];
+		}
+
+	}
+}

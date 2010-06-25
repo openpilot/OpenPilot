d29719032776ed754d24bed97c69f228cf188d2b
Mansard <nmansard@laas.fr>
2010-06-25 16:22:27 +0200

    Rebuild DMA using the new graph scheme.

--- include/rtslam/dataManagerAbstract.hpp
+++ include/rtslam/dataManagerAbstract.hpp
@@ -1,7 +1,7 @@
 /*
  * \file dataManagerAbstract.hpp
  *
- *  Created on: Apr 22, 2010
+ *  Created on: June 22, 2010
  *      Author: nmansard
  *      \ingroup rtslam
  */
@@ -11,12 +11,12 @@
 
 #include "rtslam/parents.hpp"
 #include "rtslam/sensorAbstract.hpp"
+#include "rtslam/mapManager.hpp"
 
 namespace jafar {
   namespace rtslam {
 
     class ObservationAbstract;
-
 
     /**
      * \ingroup rtslam
@@ -24,6 +24,7 @@ namespace jafar {
     class DataManagerAbstract
       : public ParentOf<ObservationAbstract>
       , public ChildOf<SensorAbstract>
+      , public ChildOf<MapManagerAbstract>
       , public boost::enable_shared_from_this<DataManagerAbstract>
     {
     public:
@@ -31,39 +32,16 @@ namespace jafar {
       ENABLE_LINK_TO_PARENT(SensorAbstract,Sensor,DataManagerAbstract);
       // define the functions sensorPtr() and sensor().
       ENABLE_ACCESS_TO_PARENT(SensorAbstract,sensor);
+      // define the function linkToParentMapManager().
+      ENABLE_LINK_TO_PARENT(MapManagerAbstract,MapManager,DataManagerAbstract);
+      // define the functions mapManagerPtr() and mapManager().
+      ENABLE_ACCESS_TO_PARENT(MapManagerAbstract,mapManager);
       // define the type ObservationList, and the function observationList().
       ENABLE_ACCESS_TO_CHILDREN(ObservationAbstract,Observation,observation);
 
       virtual ~DataManagerAbstract(void) {}
 
-      virtual void processData( boost::shared_ptr<RawAbstract> data ) = 0;
-    };
-
-
-    template< class RawSpec,class ObservationSpec,class SensorSpec,class LandmarkSpec >
-    class DataManagerActiveSearch
-      : public DataManagerAbstract
-      , public SpecificChildOf<SensorSpec>
-    {
-    protected:
-      std::list< boost::weak_ptr<ObservationSpec> > observationSpecList;
-
-    public:
-      // Define the function linkToParentSensorSpec.
-      ENABLE_LINK_TO_SPECIFIC_PARENT(SensorAbstract,SensorSpec,
-				     SensorSpec,DataManagerAbstract);
-      // Define the functions sensorSpec() and sensorSpecPtr().
-      ENABLE_ACCESS_TO_SPECIFIC_PARENT(SensorSpec,sensorSpec);
-
-      void createObservation( boost::shared_ptr<LandmarkSpec> lmkPtr )
-      {
-	boost::shared_ptr<ObservationSpec> obs_ptr
-	  ( new ObservationSpec(sensorSpecPtr(),lmkPtr) );
-	obs_ptr->linkToParentDataManager( shared_from_this() );
-	observationSpecList.push_back( obs_ptr );
-	obs_ptr->linkToSensor( sensorSpecPtr() );
-      }
-
+      virtual void process( boost::shared_ptr<RawAbstract> data ) = 0;
     };
 
   }

611aee73ad60be478aab962d2dbd947249a0a600
pip <pip>
2010-08-04 17:35:48 +0000

    Removed I2C support from PipBee as it's not used. Added 4 LED definitions.
    git-svn-id: svn://svn.openpilot.org/OpenPilot/trunk@1208 ebee16cc-31ac-478f-84a7-5cbb03baadba

--- flight/PipBee/inc/pios_config.h
+++ flight/PipBee/inc/pios_config.h
@@ -31,7 +31,6 @@
 /* Enable/Disable PiOS Modules */
 #define PIOS_INCLUDE_ADC
 #define PIOS_INCLUDE_DELAY
-#define PIOS_INCLUDE_I2C
 #define PIOS_INCLUDE_IRQ
 #define PIOS_INCLUDE_LED
 #define PIOS_INCLUDE_SPI
--- flight/PipBee/pipbee.c
+++ flight/PipBee/pipbee.c
@@ -38,35 +38,54 @@
 */
 int main()
 {
-  /* Brings up System using CMSIS functions, enables the LEDs. */
-  PIOS_SYS_Init();
-  
-  /* Delay system */
-  PIOS_DELAY_Init();
-  
-  /* Communication system */
-  PIOS_COM_Init();
-  
-  /* ADC system */
-  PIOS_ADC_Init();
-  
-  /* Magnetic sensor system */
-  PIOS_I2C_Init();
-  
-  /* SPI link to master */
-  PIOS_SPI_Init();
-   
-  // Main loop
-  while (1) {
-    uint8_t loop_ctr;
+	uint32_t loop_ctr = 0;
+	uint32_t loop_ctr2 = 0;
 
-    // Alive signal
-    if (loop_ctr++ > 100) {
-      PIOS_LED_Toggle(LED1);
-      loop_ctr = 0;
-    }
-      
-  }
+	// Brings up System using CMSIS functions, enables the LEDs.
+	PIOS_SYS_Init();
   
-  return 0;
+	// Delay system
+	PIOS_DELAY_Init();
+  
+	// Communication system
+	PIOS_COM_Init();
+  
+	// ADC system
+	PIOS_ADC_Init();
+  
+	// SPI link to master
+	PIOS_SPI_Init();
+   
+	// Main loop
+	while (1)
+	{
+		if (++loop_ctr > 10000000)
+		{
+			loop_ctr = 0;
+
+			if (++loop_ctr2 > 3) loop_ctr2 = 0;
+
+			switch (loop_ctr2)
+			{
+				case 0:
+					PIOS_LED_Toggle(LED1);
+					break;
+				case 1:
+					PIOS_LED_Toggle(LED2);
+					break;
+				case 2:
+					PIOS_LED_Toggle(LED3);
+					break;
+				case 3:
+					PIOS_LED_Toggle(LED4);
+					break;
+				default:
+					loop_ctr2 = 0;
+					PIOS_LED_Toggle(LED1);
+					break;
+			}
+		}
+	}
+  
+	return 0;
 }

e61fe4cff6a7eb9f23823959e8d6647807d256d4
peabody124 <peabody124>
2010-08-27 21:25:12 +0000

    Flight: Sorry forgot some files
    git-svn-id: svn://svn.openpilot.org/OpenPilot/trunk@1440 ebee16cc-31ac-478f-84a7-5cbb03baadba

---
+++ ground/src/plugins/uavobjects/gpstime.h
@@ -0,0 +1,88 @@
+/**
+ ******************************************************************************
+ *
+ * @file       gpstime.h
+ * @author     The OpenPilot Team, http://www.openpilot.org Copyright (C) 2010.
+ * @see        The GNU Public License (GPL) Version 3
+ * @addtogroup GCSPlugins GCS Plugins
+ * @{
+ * @addtogroup UAVObjectsPlugin UAVObjects Plugin
+ * @{
+ *   
+ * @note       Object definition file: gpstime.xml. 
+ *             This is an automatically generated file.
+ *             DO NOT modify manually.
+ *
+ * @brief      The UAVUObjects GCS plugin 
+ *****************************************************************************/
+/* 
+ * This program is free software; you can redistribute it and/or modify 
+ * it under the terms of the GNU General Public License as published by 
+ * the Free Software Foundation; either version 3 of the License, or 
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but 
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY 
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License 
+ * for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along 
+ * with this program; if not, write to the Free Software Foundation, Inc., 
+ * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ */
+#ifndef GPSTIME_H
+#define GPSTIME_H
+
+#include "uavdataobject.h"
+#include "uavobjectmanager.h"
+
+class UAVOBJECTS_EXPORT GPSTime: public UAVDataObject
+{
+    Q_OBJECT
+
+public:
+    // Field structure
+    typedef struct {
+        qint8 Month;
+        qint8 Day;
+        qint16 Year;
+        qint8 Hour;
+        qint8 Minute;
+        qint8 Second;
+
+    } __attribute__((packed)) DataFields;
+
+    // Field information
+    // Field Month information
+    // Field Day information
+    // Field Year information
+    // Field Hour information
+    // Field Minute information
+    // Field Second information
+
+  
+    // Constants
+    static const quint32 OBJID = 1459613858U;
+    static const QString NAME;
+    static const bool ISSINGLEINST = 1;
+    static const bool ISSETTINGS = 0;
+    static const quint32 NUMBYTES = sizeof(DataFields);
+
+    // Functions
+    GPSTime();
+
+    DataFields getData();
+    void setData(const DataFields& data);
+    Metadata getDefaultMetadata();
+    UAVDataObject* clone(quint32 instID);
+
+    static GPSTime* GetInstance(UAVObjectManager* objMngr, quint32 instID = 0);
+	
+private:
+    DataFields data;
+
+    void setDefaultFieldValues();
+
+};
+
+#endif // GPSTIME_H

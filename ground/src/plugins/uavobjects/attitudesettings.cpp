f9a0b799a28fa215107ef28cbf486248cc6d56b1
peabody124 <peabody124>
2010-09-04 22:01:11 +0000

    Foolishly forgot object like I always do
    git-svn-id: svn://svn.openpilot.org/OpenPilot/trunk@1529 ebee16cc-31ac-478f-84a7-5cbb03baadba

---
+++ ground/src/plugins/uavobjects/attitudesettings.cpp
@@ -0,0 +1,133 @@
+/**
+ ******************************************************************************
+ *
+ * @file       attitudesettings.cpp
+ * @author     The OpenPilot Team, http://www.openpilot.org Copyright (C) 2010.
+ * @see        The GNU Public License (GPL) Version 3
+ * @addtogroup GCSPlugins GCS Plugins
+ * @{
+ * @addtogroup UAVObjectsPlugin UAVObjects Plugin
+ * @{
+ *   
+ * @note       Object definition file: attitudesettings.xml. 
+ *             This is an automatically generated file.
+ *             DO NOT modify manually.
+ *
+ * @brief      The UAVUObjects GCS plugin
+ *****************************************************************************/
+/* 
+ * This program is free software; you can redistribute it and/or modify 
+ * it under the terms of the GNU General Public License as published by 
+ * the Free Software Foundation; either version 3 of the License, or 
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but 
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY 
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License 
+ * for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along 
+ * with this program; if not, write to the Free Software Foundation, Inc., 
+ * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ */
+#include "attitudesettings.h"
+#include "uavobjectfield.h"
+
+const QString AttitudeSettings::NAME = QString("AttitudeSettings");
+
+/**
+ * Constructor
+ */
+AttitudeSettings::AttitudeSettings(): UAVDataObject(OBJID, ISSINGLEINST, ISSETTINGS, NAME)
+{
+    // Create fields
+    QList<UAVObjectField*> fields;
+    QStringList PitchBiasElemNames;
+    PitchBiasElemNames.append("0");
+    fields.append( new UAVObjectField(QString("PitchBias"), QString(""), UAVObjectField::FLOAT32, PitchBiasElemNames, QStringList()) );
+    QStringList RollBiasElemNames;
+    RollBiasElemNames.append("0");
+    fields.append( new UAVObjectField(QString("RollBias"), QString(""), UAVObjectField::FLOAT32, RollBiasElemNames, QStringList()) );
+
+    // Initialize object
+    initializeFields(fields, (quint8*)&data, NUMBYTES);
+    // Set the default field values
+    setDefaultFieldValues();
+}
+
+/**
+ * Get the default metadata for this object
+ */
+UAVObject::Metadata AttitudeSettings::getDefaultMetadata()
+{
+    UAVObject::Metadata metadata;
+    metadata.flightAccess = ACCESS_READWRITE;
+    metadata.gcsAccess = ACCESS_READWRITE;
+    metadata.gcsTelemetryAcked = 1;
+    metadata.gcsTelemetryUpdateMode = UAVObject::UPDATEMODE_ONCHANGE;
+    metadata.gcsTelemetryUpdatePeriod = 0;
+    metadata.flightTelemetryAcked = 1;
+    metadata.flightTelemetryUpdateMode = UAVObject::UPDATEMODE_ONCHANGE;
+    metadata.flightTelemetryUpdatePeriod = 0;
+    metadata.loggingUpdateMode = UAVObject::UPDATEMODE_NEVER;
+    metadata.loggingUpdatePeriod = 0;
+    return metadata;
+}
+
+/**
+ * Initialize object fields with the default values.
+ * If a default value is not specified the object fields
+ * will be initialized to zero.
+ */
+void AttitudeSettings::setDefaultFieldValues()
+{
+    data.PitchBias = 0;
+    data.RollBias = 0;
+
+}
+
+/**
+ * Get the object data fields
+ */
+AttitudeSettings::DataFields AttitudeSettings::getData()
+{
+    QMutexLocker locker(mutex);
+    return data;
+}
+
+/**
+ * Set the object data fields
+ */
+void AttitudeSettings::setData(const DataFields& data)
+{
+    QMutexLocker locker(mutex);
+    // Get metadata
+    Metadata mdata = getMetadata();
+    // Update object if the access mode permits
+    if ( mdata.gcsAccess == ACCESS_READWRITE )
+    {
+        this->data = data;
+        emit objectUpdatedAuto(this); // trigger object updated event
+        emit objectUpdated(this);
+    }
+}
+
+/**
+ * Create a clone of this object, a new instance ID must be specified.
+ * Do not use this function directly to create new instances, the
+ * UAVObjectManager should be used instead.
+ */
+UAVDataObject* AttitudeSettings::clone(quint32 instID)
+{
+    AttitudeSettings* obj = new AttitudeSettings();
+    obj->initialize(instID, this->getMetaObject());
+    return obj;
+}
+
+/**
+ * Static function to retrieve an instance of the object.
+ */
+AttitudeSettings* AttitudeSettings::GetInstance(UAVObjectManager* objMngr, quint32 instID)
+{
+    return dynamic_cast<AttitudeSettings*>(objMngr->getObject(AttitudeSettings::OBJID, instID));
+}

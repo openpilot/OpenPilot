5dae137af0f4825721e58eb9709258432b250882
martin <martin>
2010-10-06 01:54:59 +0000

    OpenPilot.wxs divided into parts
    git-svn-id: svn://svn.openpilot.org/OpenPilot/trunk@1889 ebee16cc-31ac-478f-84a7-5cbb03baadba

--- ground/packaging/winx86/MakeInstallers.bat
+++ ground/packaging/winx86/MakeInstallers.bat
@@ -13,6 +13,6 @@ set WixBin=C:\Program Files\Windows Installer XML v3\bin
 "%WixBin%\light.exe" -nologo OpenPilot.wixobj -cultures:de-de -loc UI_translations\de-de.wxl -dWixUILicenseRtf=license\GPLv3_de.rtf -out OpenPilot_de.msi -ext WixUtilExtension  -ext WixUIExtension
 "%WixBin%\light.exe" -nologo OpenPilot.wixobj -cultures:es-es -loc UI_translations\es-es.wxl -dWixUILicenseRtf=license\GPLv3_es.rtf -out OpenPilot_es.msi -ext WixUtilExtension  -ext WixUIExtension
 "%WixBin%\light.exe" -nologo OpenPilot.wixobj -cultures:fr-fr -loc UI_translations\fr-fr.wxl -dWixUILicenseRtf=license\GPLv3_fr.rtf -out OpenPilot_fr.msi -ext WixUtilExtension  -ext WixUIExtension
-REM "%WixBin%\light.exe" -nologo OpenPilot.wixobj -cultures:zh_cn -loc zh_cn.wxl -dWixUILicenseRtf=license\GPLv3_zh_cn.rtf -out OpenPilot_zh_ch.msi -ext WixUtilExtension  -ext WixUIExtension
+REM "%WixBin%\light.exe" -nologo OpenPilot.wixobj -cultures:zh-tw -loc UI_translations\zh_cn.wxl -dWixUILicenseRtf=license\GPLv3_zh_cn.rtf -out OpenPilot_zh_ch.msi -ext WixUtilExtension  -ext WixUIExtension
 
 

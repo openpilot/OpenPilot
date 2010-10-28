42a02e566287be46155441b9dd3e4a11317bb4a6
peabody124 <peabody124>
2010-10-28 15:07:07 +0000

    Flight/HID: USB priority to 8 instead of 1.  Probably Need to make the HID layer use a similar driver format to the PIOS_USART system.  (p.s. are you happy now, PT?)
    git-svn-id: svn://svn.openpilot.org/OpenPilot/trunk@2029 ebee16cc-31ac-478f-84a7-5cbb03baadba

--- flight/PiOS/STM32F10x/pios_usb_hid.c
+++ flight/PiOS/STM32F10x/pios_usb_hid.c
@@ -81,7 +81,7 @@ int32_t PIOS_USB_HID_Init(uint32_t mode)
 	NVIC_InitTypeDef NVIC_InitStructure;
 	NVIC_PriorityGroupConfig(NVIC_PriorityGroup_2);
 	NVIC_InitStructure.NVIC_IRQChannel = USB_LP_CAN1_RX0_IRQn;
-	NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 1;
+	NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 8;
 	NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0;
 	NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
 	NVIC_Init(&NVIC_InitStructure);

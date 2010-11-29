8532bdfe2832f2ca086fbe64262b2222df3d22a1
scott <scott>
2010-11-29 23:34:03 +0000

    An example batch file for starting Flightgear as a stand-alone app on Windows.
    git-svn-id: svn://svn.openpilot.org/OpenPilot/trunk@2179 ebee16cc-31ac-478f-84a7-5cbb03baadba

---
+++ ground/src/plugins/hitlnew/Start Flightgear XP.bat
@@ -0,0 +1,2 @@
+cd "C:\Program Files\FlightGear\bin\Win32"
+"C:\Program Files\FlightGear\bin\Win32\fgfs.exe" --fg-root="c:\program files\flightgear\data" --timeofday=noon --httpd=5400 --enable-hud --in-air --altitude=3000 --vc=100 --log-level=alert --generic=socket,out,20,192.168.1.109,5500,udp,opfgprotocol --generic=socket,in,80,192.168.1.115,10001,udp,opfgprotocol"

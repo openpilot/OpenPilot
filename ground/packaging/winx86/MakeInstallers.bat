be4df7b757bc4b1e116641fb573f1fb5fdc3643b
martin <martin>
2010-12-22 23:31:31 +0000

    changed scripts and added tools for creating multilingual installer
    git-svn-id: svn://svn.openpilot.org/OpenPilot/trunk@2275 ebee16cc-31ac-478f-84a7-5cbb03baadba

--- ground/packaging/winx86/MakeInstallers.bat
+++ ground/packaging/winx86/MakeInstallers.bat
@@ -8,11 +8,24 @@ set WixBin=C:\Program Files\Windows Installer XML v3\bin
 
 "%WixBin%\candle.exe" -nologo "C:\Projects\builds\OpenPilot_Release\packaging\winx86\OpenPilot.wxs" -out OpenPilot.wixobj  -ext WixUtilExtension  -ext WixUIExtension
 
-"%WixBin%\light.exe" -nologo OpenPilot.wixobj -cultures:en-us -loc UI_translations\en-us.wxl -dWixUILicenseRtf=license\GPLv3_en.rtf -out OpenPilot_en.msi -ext WixUtilExtension  -ext WixUIExtension
-"%WixBin%\light.exe" -nologo OpenPilot.wixobj -cultures:ru-ru -loc UI_translations\ru-ru.wxl -dWixUILicenseRtf=license\GPLv3_ru.rtf -out OpenPilot_ru.msi -ext WixUtilExtension  -ext WixUIExtension
-"%WixBin%\light.exe" -nologo OpenPilot.wixobj -cultures:de-de -loc UI_translations\de-de.wxl -dWixUILicenseRtf=license\GPLv3_de.rtf -out OpenPilot_de.msi -ext WixUtilExtension  -ext WixUIExtension
-"%WixBin%\light.exe" -nologo OpenPilot.wixobj -cultures:es-es -loc UI_translations\es-es.wxl -dWixUILicenseRtf=license\GPLv3_es.rtf -out OpenPilot_es.msi -ext WixUtilExtension  -ext WixUIExtension
-"%WixBin%\light.exe" -nologo OpenPilot.wixobj -cultures:fr-fr -loc UI_translations\fr-fr.wxl -dWixUILicenseRtf=license\GPLv3_fr.rtf -out OpenPilot_fr.msi -ext WixUtilExtension  -ext WixUIExtension
-"%WixBin%\light.exe" -nologo OpenPilot.wixobj -cultures:zh-tw -loc UI_translations\zh_cn.wxl -loc UI_translations\wixui_zh-tw.wxl -dWixUILicenseRtf=license\GPLv3_zh_cn.rtf -out OpenPilot_zh_ch.msi -ext WixUtilExtension  -ext WixUIExtension
+"%WixBin%\light.exe" -nologo OpenPilot.wixobj -sval -spdb -cultures:en-us -loc UI_translations\en-us.wxl -dWixUILicenseRtf=license\GPLv3_en.rtf -out OpenPilot.msi -ext WixUtilExtension  -ext WixUIExtension
+"%WixBin%\light.exe" -nologo OpenPilot.wixobj -sval -spdb -cultures:ru-ru -loc UI_translations\ru-ru.wxl -dWixUILicenseRtf=license\GPLv3_ru.rtf -out OpenPilot_ru.msi -ext WixUtilExtension  -ext WixUIExtension
+"%WixBin%\light.exe" -nologo OpenPilot.wixobj -sval -spdb -cultures:de-de -loc UI_translations\de-de.wxl -dWixUILicenseRtf=license\GPLv3_de.rtf -out OpenPilot_de.msi -ext WixUtilExtension  -ext WixUIExtension
+"%WixBin%\light.exe" -nologo OpenPilot.wixobj -sval -spdb -cultures:es-es -loc UI_translations\es-es.wxl -dWixUILicenseRtf=license\GPLv3_es.rtf -out OpenPilot_es.msi -ext WixUtilExtension  -ext WixUIExtension
+"%WixBin%\light.exe" -nologo OpenPilot.wixobj -sval -spdb -cultures:fr-fr -loc UI_translations\fr-fr.wxl -dWixUILicenseRtf=license\GPLv3_fr.rtf -out OpenPilot_fr.msi -ext WixUtilExtension  -ext WixUIExtension
+"%WixBin%\light.exe" -nologo OpenPilot.wixobj -sval -spdb -cultures:zh-tw -loc UI_translations\zh_cn.wxl -loc UI_translations\wixui_zh-tw.wxl -dWixUILicenseRtf=license\GPLv3_zh_cn.rtf -out OpenPilot_zh_ch.msi -ext WixUtilExtension  -ext WixUIExtension
 
 
+MsiTools\MsiTran.exe -g OpenPilot.msi OpenPilot_ru.msi ru.mst
+MsiTools\MsiTran.exe -g OpenPilot.msi OpenPilot_de.msi de.mst
+MsiTools\MsiTran.exe -g OpenPilot.msi OpenPilot_es.msi es.mst
+MsiTools\MsiTran.exe -g OpenPilot.msi OpenPilot_fr.msi fr.mst
+MsiTools\MsiTran.exe -g OpenPilot.msi OpenPilot_zh_ch.msi zn.mst
+
+MsiTools\wisubstg.vbs OpenPilot.msi ru.mst 1049
+MsiTools\wisubstg.vbs OpenPilot.msi de.mst 1031
+MsiTools\wisubstg.vbs OpenPilot.msi es.mst 3082
+MsiTools\wisubstg.vbs OpenPilot.msi fr.mst 1036
+MsiTools\wisubstg.vbs OpenPilot.msi zn.mst 1028
+
+del OpenPilot.wixobj OpenPilot_ru.msi ru.mst OpenPilot_de.msi de.mst OpenPilot_es.msi es.mst OpenPilot_fr.msi fr.mst OpenPilot_zh_ch.msi zn.mst

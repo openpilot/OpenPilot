924716587ec4e5e8c29fb9bfad5be67535fdffac
Nizar Sallem <nksallem@laas.fr>
2011-01-05 14:02:59 +0100

    Fix the jann interface and add a jann demo
    Replace: paired.data with car0.ascii to avoid files wasting in file_demo
    Add: jann_demo.cpp for a demo of jafar flann interface
    Fix: jann interface and add checks to be consistent with flann
    Add: in jann interface add function calls that use a single query

---
+++ demo_suite/jann_demo.cpp
@@ -0,0 +1,25 @@
+////////////////////////////////////////////////////////////////////
+// The car files are courtesy of Alex Segal found in gicp package //
+////////////////////////////////////////////////////////////////////
+#include "jmath/jann.hpp"
+#include "jmath/mat_file.hpp"
+
+using namespace jafar;
+using namespace std;
+using namespace jann;
+int main(int argc, char** argv) {
+	jmath::matrix_file<float> model;
+	model.load("car0.ascii");
+	for (unsigned int r = 0; r < model.data.size1(); r++) {
+		for (unsigned int c = 0; c < model.data.size2(); c++)
+			cout << model.data(r,c)<< " ";
+		cout << endl;
+	}
+	jmath::matrix_file<float> scene;
+	scene.load("car1.ascii");
+	ublas::matrix<float> dists(scene.data.size1(), scene.data.size2());
+	ublas::matrix<int> indices(scene.data.size1(), scene.data.size2());
+	KD_tree_index< flann::L2<float> > tree(model.data);
+	tree.build();
+	tree.knn_search(scene.data, indices, dists, 10, search_params(32));
+}

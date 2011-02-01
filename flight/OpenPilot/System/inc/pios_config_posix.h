c3aa5ed8f3a3959ab4f99a559fd2641387fcb2cf
peabody124 <peabody124>
2011-02-01 15:47:02 +0000

    Posix: Add the constants for telemetry queue sizes and also make the actuator SetHz calls compatible with the PiOS API change
    git-svn-id: svn://svn.openpilot.org/OpenPilot/trunk@2672 ebee16cc-31ac-478f-84a7-5cbb03baadba

--- flight/OpenPilot/System/inc/pios_config_posix.h
+++ flight/OpenPilot/System/inc/pios_config_posix.h
@@ -54,5 +54,6 @@
 #define SERVOS_POSITION_MIN		800
 #define SERVOS_POSITION_MAX		2200
 
-
+#define TELEM_QUEUE_SIZE                20
+#define TELEM_STACK_SIZE                2048
 #endif /* PIOS_CONFIG_POSIX_H */

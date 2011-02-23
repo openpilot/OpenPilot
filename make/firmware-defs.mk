6700a74782379da2135eac395efb76dd0ec88cda
stac <stac>
2011-02-23 04:18:53 +0000

    makefile: refactor makefiles and clean up output
    This significantly reduces the amount of duplication
    across the various firmware makefiles.
    
    The new firmware-defs.mk file should contain only
    macros/declarations that will apply to all firmware
    makefiles.
    
    git-svn-id: svn://svn.openpilot.org/OpenPilot/trunk@2854 ebee16cc-31ac-478f-84a7-5cbb03baadba

---
+++ make/firmware-defs.mk
@@ -0,0 +1,145 @@
+# Toolchain prefix (i.e arm-elf- -> arm-elf-gcc.exe)
+TCHAIN_PREFIX ?= arm-none-eabi-
+
+# Define toolchain component names.
+CC      = $(TCHAIN_PREFIX)gcc
+CPP     = $(TCHAIN_PREFIX)g++
+AR      = $(TCHAIN_PREFIX)ar
+OBJCOPY = $(TCHAIN_PREFIX)objcopy
+OBJDUMP = $(TCHAIN_PREFIX)objdump
+SIZE    = $(TCHAIN_PREFIX)size
+NM      = $(TCHAIN_PREFIX)nm
+
+THUMB   = -mthumb
+
+# Test if quotes are needed for the echo-command
+result = ${shell echo "test"}
+ifeq (${result}, test)
+	quote = '
+# This line is just to clear out the single quote above '
+else
+	quote =
+endif
+
+# Define Messages
+# English
+MSG_FORMATERROR      := ${quote} Can not handle output-format${quote}
+MSG_MODINIT          := ${quote} MODINIT   ${quote}
+MSG_SIZE             := ${quote} SIZE      ${quote}
+MSG_LOAD_FILE        := ${quote} BIN/HEX   ${quote}
+MSG_EXTENDED_LISTING := ${quote} LIS       ${quote}
+MSG_SYMBOL_TABLE     := ${quote} NM        ${quote}
+MSG_LINKING          := ${quote} LD        ${quote}
+MSG_COMPILING        := ${quote} CC        ${quote}
+MSG_COMPILING_ARM    := ${quote} CC-ARM    ${quote}
+MSG_COMPILINGCPP     := ${quote} CXX       ${quote}
+MSG_COMPILINGCPP_ARM := ${quote} CXX-ARM   ${quote}
+MSG_ASSEMBLING       := ${quote} AS        ${quote}
+MSG_ASSEMBLING_ARM   := ${quote} AS-ARM    ${quote}
+MSG_CLEANING         := ${quote} CLEAN     ${quote}
+MSG_ASMFROMC         := ${quote} AS(C)     ${quote}
+MSG_ASMFROMC_ARM     := ${quote} AS(C)-ARM ${quote}
+MSG_PYMITEINIT       := ${quote} PY        ${quote}
+
+toprel = $(subst $(realpath $(TOP))/,,$(abspath $(1)))
+
+# Display compiler version information.
+.PHONY: gccversion
+gccversion :
+	@$(CC) --version
+
+# Create final output file (.hex) from ELF output file.
+%.hex: %.elf
+	@echo $(MSG_LOAD_FILE) $(call toprel, $@)
+	$(V1) $(OBJCOPY) -O ihex $< $@
+
+# Create final output file (.bin) from ELF output file.
+%.bin: %.elf
+	@echo $(MSG_LOAD_FILE) $(call toprel, $@)
+	$(V1) $(OBJCOPY) -O binary $< $@
+
+# Create extended listing file/disassambly from ELF output file.
+# using objdump testing: option -C
+%.lss: %.elf
+	@echo $(MSG_EXTENDED_LISTING) $(call toprel, $@)
+	$(V1) $(OBJDUMP) -h -S -C -r $< > $@
+
+# Create a symbol table from ELF output file.
+%.sym: %.elf
+	@echo $(MSG_SYMBOL_TABLE) $(call toprel, $@)
+	$(V1) $(NM) -n $< > $@
+
+define SIZE_TEMPLATE
+.PHONY: $(1)_size
+$(1)_size: $(1)
+	@echo $(MSG_SIZE) $$(call toprel, $$<)
+	$(V1) $(SIZE) -A $$<
+endef
+
+# Assemble: create object files from assembler source files.
+define ASSEMBLE_TEMPLATE
+$(OUTDIR)/$(notdir $(basename $(1))).o : $(1)
+	@echo $(MSG_ASSEMBLING) $$(call toprel, $$<)
+	$(V1) $(CC) -c -mthumb $$(ASFLAGS) $$< -o $$@
+endef
+
+# Assemble: create object files from assembler source files. ARM-only
+define ASSEMBLE_ARM_TEMPLATE
+$(OUTDIR)/$(notdir $(basename $(1))).o : $(1)
+	@echo $(MSG_ASSEMBLING_ARM) $$(call toprel, $$<)
+	$(V1) $(CC) -c $$(ASFLAGS) $$< -o $$@
+endef
+
+# Compile: create object files from C source files.
+define COMPILE_C_TEMPLATE
+$(OUTDIR)/$(notdir $(basename $(1))).o : $(1)
+	@echo $(MSG_COMPILING) $$(call toprel, $$<)
+	$(V1) $(CC) -c -mthumb $$(CFLAGS) $$(CONLYFLAGS) $$< -o $$@
+endef
+
+# Compile: create object files from C source files. ARM-only
+define COMPILE_C_ARM_TEMPLATE
+$(OUTDIR)/$(notdir $(basename $(1))).o : $(1)
+	@echo $(MSG_COMPILING_ARM) $$(call toprel, $$<)
+	$(V1) $(CC) -c $$(CFLAGS) $$(CONLYFLAGS) $$< -o $$@
+endef
+
+# Compile: create object files from C++ source files.
+define COMPILE_CPP_TEMPLATE
+$(OUTDIR)/$(notdir $(basename $(1))).o : $(1)
+	@echo $(MSG_COMPILINGCPP) $$(call toprel, $$<)
+	$(V1) $(CC) -c -mthumb $$(CFLAGS) $$(CPPFLAGS) $$< -o $$@
+endef
+
+# Compile: create object files from C++ source files. ARM-only
+define COMPILE_CPP_ARM_TEMPLATE
+$(OUTDIR)/$(notdir $(basename $(1))).o : $(1)
+	@echo $(MSG_COMPILINGCPP_ARM) $$(call toprel, $$<)
+	$(V1) $(CC) -c $$(CFLAGS) $$(CPPFLAGS) $$< -o $$@
+endef
+
+# Link: create ELF output file from object files.
+#   $1 = elf file to produce
+#   $2 = list of object files that make up the elf file
+define LINK_TEMPLATE
+.SECONDARY : $(1)
+.PRECIOUS : $(2)
+$(1):  $(2)
+	@echo $(MSG_LINKING) $$(call toprel, $$@)
+	$(V1) $(CC) -mthumb $$(CFLAGS) $(2) --output $$@ $$(LDFLAGS)
+endef
+
+# Compile: create assembler files from C source files. ARM/Thumb
+define PARTIAL_COMPILE_TEMPLATE
+$($(1):.c=.s) : %.s : %.c
+	@echo $(MSG_ASMFROMC) $$(call toprel, $$<)
+	$(V1) $(CC) -mthumb -S $$(CFLAGS) $$(CONLYFLAGS) $$< -o $$@
+endef
+
+# Compile: create assembler files from C source files. ARM only
+define PARTIAL_COMPILE_ARM_TEMPLATE
+$($(1):.c=.s) : %.s : %.c
+	@echo $(MSG_ASMFROMC_ARM) $$(call toprel, $$<)
+	$(V1) $(CC) -S $$(CFLAGS) $$(CONLYFLAGS) $$< -o $$@
+endef
+

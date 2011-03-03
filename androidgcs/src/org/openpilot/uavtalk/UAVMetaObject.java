0abd2ee6719138e3dc36d2ee1117d935c877acdd
James Cotton <peabody124@gmail.com>
2011-03-02 20:23:27 -0600

    Continuing to work on the java code to be more consistent with GCS code

--- androidgcs/src/org/openpilot/uavtalk/UAVMetaObject.java
+++ androidgcs/src/org/openpilot/uavtalk/UAVMetaObject.java
@@ -2,23 +2,41 @@ package org.openpilot.uavtalk;
 
 public class UAVMetaObject extends UAVObject {
 
-	private UAVDataObject parent;
-	
-	public UAVMetaObject(int objID, String mname, UAVDataObject parent) {
-		super(objID, true, mname);
+	public UAVMetaObject(int objID, String name, UAVDataObject parent) {
+		super(objID, true, name);
 		this.parent = parent;
+		
+	    ownMetadata.flightAccess = UAVObject.AccessMode.ACCESS_READWRITE;
+	    ownMetadata.gcsAccess = UAVObject.AccessMode.ACCESS_READWRITE;
+	    ownMetadata.flightTelemetryAcked = UAVObject.Acked.TRUE;
+	    ownMetadata.flightTelemetryUpdateMode = UAVObject.UpdateMode.UPDATEMODE_ONCHANGE;
+	    ownMetadata.flightTelemetryUpdatePeriod = 0;
+	    ownMetadata.gcsTelemetryAcked = UAVObject.Acked.TRUE;
+	    ownMetadata.gcsTelemetryUpdateMode = UAVObject.UpdateMode.UPDATEMODE_ONCHANGE;
+	    ownMetadata.gcsTelemetryUpdatePeriod = 0;
+	    ownMetadata.loggingUpdateMode = UAVObject.UpdateMode.UPDATEMODE_ONCHANGE;
+	    ownMetadata.loggingUpdatePeriod = 0;
+		
+	}
+
+	UAVObject getParentObject() {
+		return parent;
 	}
 
 	@Override
 	public void deserialize(byte[] arr, int offset) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
-	public UAVMetaObject getDefaultMetadata() {
-		// TODO Auto-generated method stub
-		return null;
+	public Metadata getMetadata() {
+		return ownMetadata;
+	}
+
+	@Override
+	public Metadata getDefaultMetadata() {
+	    return ownMetadata;
 	}
 
 	@Override
@@ -44,5 +62,18 @@ public class UAVMetaObject extends UAVObject {
 		// TODO Auto-generated method stub
 		return null;
 	}
+
+	
+	public UAVMetaObject(quint32 objID, const QString& name, UAVObject* parent);
+	UAVObject* getParentObject();
+	void setMetadata(const Metadata& mdata);
+	void setData(const Metadata& mdata);
+	Metadata getData();
+
+	
+	private UAVObject parent;
+	private Metadata ownMetadata;
+	private Metadata parentMetadata;
+
 
 }
--- ground/openpilotgcs/src/libs/juavobjects/templates/uavobjecttemplate.java
+++ ground/openpilotgcs/src/libs/juavobjects/templates/uavobjecttemplate.java
@@ -31,29 +31,34 @@ package org.openpilot.uavtalk.uavobjects;
 import  org.openpilot.uavtalk.UAVObject;
 import  org.openpilot.uavtalk.UAVDataObject;
 import  org.openpilot.uavtalk.UAVMetaObject;
+import  org.openpilot.uavtalk.UAVObjectField;
 
 /**
 $(DESCRIPTION)
 
 generated from $(XMLFILE)
 **/
-public class $(NAME) extends UAVDataObject{
+public class $(NAME) extends UAVDataObject {
 
 $(FIELDSINIT)
 
+    public $(NAME) (int objID, Boolean isSingleInst, Boolean isSet, String name) {
+        super(OBJID, ISSINGLEINST, ISSETTINGS, NAME);
+    }
+
     public void setGeneratedMetaData() {
 
-	getMetaData().gcsAccess = UAVMetaObject.$(GCSACCESS);
-	getMetaData().gcsTelemetryAcked = UAVMetaObject.$(GCSTELEM_ACKEDTF);
-	getMetaData().gcsTelemetryUpdateMode = UAVMetaObject.$(GCSTELEM_UPDATEMODE);
+	getMetaData().gcsAccess = UAVObject.AccessMode.$(GCSACCESS);
+	getMetaData().gcsTelemetryAcked = UAVObject.Acked.$(GCSTELEM_ACKEDTF);
+	getMetaData().gcsTelemetryUpdateMode = UAVObject.UpdateMode.$(GCSTELEM_UPDATEMODE);
 	getMetaData().gcsTelemetryUpdatePeriod = $(GCSTELEM_UPDATEPERIOD);
 
-	getMetaData().flightAccess = UAVMetaObject.$(FLIGHTACCESS);
-	getMetaData().flightTelemetryAcked = UAVMetaObject.$(FLIGHTTELEM_ACKEDTF);
-	getMetaData().flightTelemetryUpdateMode = UAVMetaObject.$(FLIGHTTELEM_UPDATEMODE);
+	getMetaData().flightAccess = UAVObject.AccessMode.$(FLIGHTACCESS);
+	getMetaData().flightTelemetryAcked = UAVObject.Acked.$(FLIGHTTELEM_ACKEDTF);
+	getMetaData().flightTelemetryUpdateMode = UAVObject.UpdateMode.$(FLIGHTTELEM_UPDATEMODE);
 	getMetaData().flightTelemetryUpdatePeriod = $(FLIGHTTELEM_UPDATEPERIOD);
 
-	getMetaData().loggingUpdateMode = UAVMetaObject.$(LOGGING_UPDATEMODE);
+	getMetaData().loggingUpdateMode = UAVObject.UpdateMode.$(LOGGING_UPDATEMODE);
 	getMetaData().loggingUpdatePeriod = $(LOGGING_UPDATEPERIOD);
 
     }
@@ -69,6 +74,15 @@ $(FIELDSINIT)
     public String getDescription() {
 	return "$(DESCRIPTION)";
     }
+protected:
+	// Constants
+	static final int OBJID = $(OBJIDHEX);
+	static final String NAME;
+	static final String DESCRIPTION;
+	static final boolean ISSINGLEINST = $(ISSINGLEINST);
+	static final boolean ISSETTINGS = $(ISSETTINGS);
+	static final int NUMBYTES = sizeof(DataFields);
+
 
 $(GETTERSETTER)
 }

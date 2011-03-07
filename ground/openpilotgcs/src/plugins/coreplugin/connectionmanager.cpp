5c7e74e1691883fb65a8ccecea1e6e197a012f37
pip <pip>
2011-03-07 22:07:41 +0000

    Removed commented out un-used signal/slot
    git-svn-id: svn://svn.openpilot.org/OpenPilot/trunk@3003 ebee16cc-31ac-478f-84a7-5cbb03baadba

--- ground/openpilotgcs/src/plugins/coreplugin/connectionmanager.cpp
+++ ground/openpilotgcs/src/plugins/coreplugin/connectionmanager.cpp
@@ -140,7 +140,6 @@ bool ConnectionManager::connectDevice()
 	m_connectionDevice = connection_device;
 	m_ioDev = io_dev;
 
-//	connect(m_connectionDevice.connection, SIGNAL(deviceClosed(QObject *)), this, SLOT(onConnectionClosed(QObject *)));
 	connect(m_connectionDevice.connection, SIGNAL(destroyed(QObject *)), this, SLOT(onConnectionDestroyed(QObject *)), Qt::QueuedConnection);
 
 	m_connectBtn->setText("Disconnect");

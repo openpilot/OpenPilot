515a4c4fd1c6cc3a033c137f806d310ab7bdfda4
James Cotton <peabody124@gmail.com>
2011-03-19 20:00:12 -0500

    Start of a widget for monitoring telemetry status

---
+++ androidgcs/src/org/openpilot/androidgcs/TelemetryWidget.java
@@ -0,0 +1,31 @@
+package org.openpilot.androidgcs;
+
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.RemoteViews;
+
+public class TelemetryWidget extends AppWidgetProvider {
+	
+	private static boolean connected = false;
+
+	public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        final int N = appWidgetIds.length;
+
+        // Perform this loop procedure for each App Widget that belongs to this provider
+        for (int i=0; i<N; i++) {
+            int appWidgetId = appWidgetIds[i];
+
+            // Get the layout for the App Widget and attach an on-click listener to the button
+            RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.telemetry_widget);
+            //views.setOnClickPendingIntent(R.id.button, pendingIntent);
+
+            // Tell the AppWidgetManager to perform an update on the current App Widget
+            appWidgetManager.updateAppWidget(appWidgetId, views);
+        }
+    }
+    
+
+}

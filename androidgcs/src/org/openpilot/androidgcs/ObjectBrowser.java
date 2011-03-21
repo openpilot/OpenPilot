68e0e27f75ed64fc3929fed6b1135d64faef5900
James Cotton <peabody124@gmail.com>
2011-03-20 21:34:03 -0500

    Added a home page, added an option to select connection type.  Made the ListView adapter trigger updates on the data.

--- androidgcs/src/org/openpilot/androidgcs/ObjectBrowser.java
+++ androidgcs/src/org/openpilot/androidgcs/ObjectBrowser.java
@@ -6,15 +6,20 @@ import java.util.ListIterator;
 import java.util.Observable;
 import java.util.Observer;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
 import android.database.DataSetObserver;
 import android.os.Bundle;
 import android.os.Handler;
+import android.preference.PreferenceManager;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Adapter;
 import android.widget.ArrayAdapter;
 import android.widget.ExpandableListAdapter;
 import android.widget.ExpandableListView;
@@ -27,36 +32,13 @@ import android.widget.Toast;
 import org.openpilot.uavtalk.UAVDataObject;
 import org.openpilot.uavtalk.UAVObject;
 
-public class ObjectBrowser extends ObjectManagerActivity {
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		switch(item.getItemId()) {
-		case R.id.menu_connect:
-			binder.openBTConnection();
-			return true;
-		case R.id.menu_disconnect:
-			binder.stopConnection();
-			updateText.run();
-			return true;
-		case R.id.menu_settings:
-			return true;
-		default:
-			return super.onOptionsItemSelected(item);
-		}
-
-	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		MenuInflater inflater = getMenuInflater();
-		inflater.inflate(R.menu.options_menu, menu);
-		return true;
-	}
+public class ObjectBrowser extends ObjectManagerActivity implements OnSharedPreferenceChangeListener {
 
 	private final String TAG = "ObjectBrower";
 	boolean connected;
-
+	SharedPreferences prefs;
+	ArrayAdapter<UAVDataObject> adapter;
+	
 	final Handler uavobjHandler = new Handler(); 
 	final Runnable updateText = new Runnable() {
 		public void run() {
@@ -70,6 +52,8 @@ public class ObjectBrowser extends ObjectManagerActivity {
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);		
+		prefs = PreferenceManager.getDefaultSharedPreferences(this);
+		prefs.registerOnSharedPreferenceChangeListener(this);
 	}
 
 	@Override
@@ -77,6 +61,17 @@ public class ObjectBrowser extends ObjectManagerActivity {
 		Toast.makeText(this,"Telemetry estabilished",Toast.LENGTH_SHORT);
 		Log.d(TAG, "onOPConnected()");
 		
+		List<List<UAVDataObject>> allobjects = objMngr.getDataObjects();
+		List<UAVDataObject> linearized = new ArrayList<UAVDataObject>();
+		ListIterator<List<UAVDataObject>> li = allobjects.listIterator();
+		while(li.hasNext()) {
+			linearized.addAll(li.next());
+		}
+		
+		adapter = new ArrayAdapter<UAVDataObject>(this,R.layout.object_view, linearized);
+		ListView objects = (ListView) findViewById(R.id.object_list);
+		objects.setAdapter(adapter);
+
 		UAVObject obj = objMngr.getObject("SystemStats");
 		if(obj != null)
 			obj.addUpdatedObserver(new Observer() {
@@ -88,16 +83,12 @@ public class ObjectBrowser extends ObjectManagerActivity {
 	}
 	
 	public void update() {
-		List<List<UAVDataObject>> allobjects = objMngr.getDataObjects();
-		List<UAVDataObject> linearized = new ArrayList<UAVDataObject>();
-		ListIterator<List<UAVDataObject>> li = allobjects.listIterator();
-		while(li.hasNext()) {
-			linearized.addAll(li.next());
-		}
-		
-		ArrayAdapter<UAVDataObject> adapter = new ArrayAdapter<UAVDataObject>(this,R.layout.object_view, linearized);
-		ListView objects = (ListView) findViewById(R.id.object_list);
-		objects.setAdapter(adapter);
+		adapter.notifyDataSetChanged();
+	}
 
+	public void onSharedPreferenceChanged(SharedPreferences sharedPreferences,
+			String key) {
+		// TODO Auto-generated method stub
+		
 	}
 }
---
+++ androidgcs/src/org/openpilot/androidgcs/Preferences.java
@@ -0,0 +1,12 @@
+package org.openpilot.androidgcs;
+
+import android.os.Bundle;
+import android.preference.PreferenceActivity;
+
+public class Preferences extends PreferenceActivity {
+	  @Override
+	  protected void onCreate(Bundle savedInstanceState) { 
+	    super.onCreate(savedInstanceState);
+	    addPreferencesFromResource(R.xml.preferences); 
+	  }
+}

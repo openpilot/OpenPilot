13807e731b9bc3b647b9f4cdd06a01893e2c658c
Cyril Roussillon <cyril.roussillon@laas.fr>
2011-03-22 00:43:25 +0100

    Fix big mistake in getNowTimestamp...

--- include/rtslam/rtSlam.hpp
+++ include/rtslam/rtSlam.hpp
@@ -61,7 +61,7 @@ namespace jafar {
 			struct timeval tv;
 			struct timezone tz;
 			gettimeofday(&tv, &tz);
-			return tv.tv_sec+tv.tv_usec/10e-6;
+			return tv.tv_sec+tv.tv_usec*1e-6;
 		}
 
 		// forward declarations

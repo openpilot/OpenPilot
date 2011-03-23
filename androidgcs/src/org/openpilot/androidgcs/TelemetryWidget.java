1a4db3332859acf2218083bdd4cc2e8db7639063
James Cotton <peabody124@gmail.com>
2011-03-22 21:59:36 -0500

    Few tweaks and suppress some warnings

--- androidgcs/src/org/openpilot/androidgcs/TelemetryWidget.java
+++ androidgcs/src/org/openpilot/androidgcs/TelemetryWidget.java
@@ -2,11 +2,34 @@ package org.openpilot.androidgcs;
 
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
+import android.content.ComponentName;
 import android.content.Context;
+import android.content.Intent;
 import android.widget.RemoteViews;
 
 public class TelemetryWidget extends AppWidgetProvider {
 	
+	@Override
+	public void onReceive(Context context, Intent intent) {
+		if(intent.getAction().equals(OPTelemetryService.INTENT_ACTION_CONNECTED)) {
+			changeStatus(context, true);
+		}
+		if(intent.getAction().equals(OPTelemetryService.INTENT_ACTION_DISCONNECTED)) {
+			changeStatus(context, false);
+		}		
+        
+		super.onReceive(context, intent);
+	}
+	
+	public void changeStatus(Context context, boolean status) {
+		RemoteViews updateViews =  new RemoteViews(context.getPackageName(), R.layout.telemetry_widget);
+		updateViews.setTextViewText(R.id.telemetryWidgetStatus, "Connection status: " + status);
+		ComponentName thisWidget = new ComponentName(context, TelemetryWidget.class);
+        AppWidgetManager manager = AppWidgetManager.getInstance(context);
+        manager.updateAppWidget(thisWidget, updateViews);
+
+	}
+
 	public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
         final int N = appWidgetIds.length;
 
@@ -16,7 +39,6 @@ public class TelemetryWidget extends AppWidgetProvider {
 
             // Get the layout for the App Widget and attach an on-click listener to the button
             RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.telemetry_widget);
-            //views.setOnClickPendingIntent(R.id.button, pendingIntent);
 
             // Tell the AppWidgetManager to perform an update on the current App Widget
             appWidgetManager.updateAppWidget(appWidgetId, views);

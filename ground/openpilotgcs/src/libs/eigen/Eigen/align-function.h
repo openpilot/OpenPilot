55cba70bd7a67ce5c154e810e0b57e178a40ed02
jonathan <jonathan>
2011-04-10 18:32:19 +0000

    spellcheck comments: no substantive changes
    git-svn-id: svn://svn.openpilot.org/OpenPilot/trunk@3149 ebee16cc-31ac-478f-84a7-5cbb03baadba

--- ground/openpilotgcs/src/libs/eigen/Eigen/align-function.h
+++ ground/openpilotgcs/src/libs/eigen/Eigen/align-function.h
@@ -4,7 +4,7 @@
 /*
  * The purpose of this macro is to force the alignment of the stack to a
  * 16-byte boundary on systems that don't provide it automatically.  At
- * this time, only GCC on Win32 is know to require it.  MSVC on Win32 may
+ * this time, only GCC on Win32 is known to require it.  MSVC on Win32 may
  * as well.
  */
 

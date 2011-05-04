6d8c5a8d130de6d88f457e481656df26b788e800
Cyril Roussillon <cyril.roussillon@laas.fr>
2011-05-04 17:13:54 +0200

    rename scripts nees to montecarlo, and update them

--- data/scripts/montecarlo_loop.sh
+++ data/scripts/montecarlo_loop.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
-NUM_FILE=nees.log
-MAX_NUM=100
+# This script executes montecarlo runs on a single machine.
+# Modify this file to setup the number of runs you want in total
+# and the SLAM options.
+# 
+# WARNING: Modify the version of this file that is actually used by the
+# machines running SLAM!
+#
+# Author: croussil
+
+
+NUM_FILE=$1
+MAX_NUM=$2
 COMMAND="demo_suite/x86_64-linux-gnu/demo_slam"
 DATA_PATH="/net/tic/data1/robots/camera/2011-02-16_motion-capture/01/"
 ARGS=" \
@@ -10,7 +20,7 @@ ARGS=" \
 	--config-estimation=@/estimation.cfg --data-path=$DATA_PATH"
 LOG_PREFIX=results
 
-#mkdir -p $DATA_PATH/$LOG_PREFIX
+mkdir -p $DATA_PATH/$LOG_PREFIX
 
 num=0
 
@@ -27,8 +37,6 @@ update_num () {
 update_num
 
 while [ "$num" -lt "$MAX_NUM" ]; do
-#	echo "nice -n 15 $COMMAND $ARGS --log=$LOG_PREFIX/rtslam_`printf '%03d' $num`.log"
-#	nice -n 15 $COMMAND $ARGS --log=$LOG_PREFIX/rtslam_`printf "%03d" $num`.log
 	$COMMAND $ARGS --log=$LOG_PREFIX/rtslam_`printf "%03d" $num`.log
 	update_num
 done
---
+++ data/scripts/montecarlo_ssh.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+
+# This script starts montecarlo runs on different machines.
+# Modify the machines they should be started on and the number of runs here.
+# Modify the montecarlo_ssh_loop.sh script to setup the SLAM options
+# (the version which will be used on the distant machines!).
+#
+# Author: croussil
+
+
+MACHINES="\
+	bugsbunny bugsbunny bugsbunny bugsbunny bugsbunny bugsbunny bugsbunny bugsbunny \
+	finot finot finot finot finot finot finot finot \
+	sioux sioux sioux sioux sioux sioux sioux sioux \
+	valse valse valse valse \
+	jive jive jive jive \
+	dummy dummy dummy dummy \
+	flex flex flex flex \
+	topaze topaze topaze topaze \
+	germanium germanium germanium germanium \
+	arsenic arsenic arsenic arsenic \
+	chrome chrome chrome chrome \
+	cobalt cobalt cobalt cobalt \
+	nickel nickel nickel \
+	bore bore bore bore \
+	calcium calcium calcium calcium \
+	gallium gallium gallium gallium \
+	silicium silicium silicium silicium \
+	azote azote azote azote azote \
+	antimoine antimoine antimoine antimoine \
+	jaune jaune jaune jaune \
+	poussin poussin poussin poussin \
+	ocre ocre ocre ocre \
+	safran safran safran safran \
+	blond blond blond blond\
+"
+
+RUNPATH=/home/croussil/Libs/jafar/build_release/modules/rtslam
+NUM_FILE=nees.log
+MAX_NUM=100
+
+if [[ "$1" -ne "0" ]]; then
+	# START
+
+	for machine in $MACHINES; do
+		ssh -4 -t $machine "cd $RUNPATH; screen -S rtslam -d -m data/scripts/montecarlo_loop.sh $NUM_FILE $MAX_NUM"
+		sleep 1
+	done
+
+else
+	# STOP
+
+	for machine in $MACHINES; do
+		ssh -4 $machine "cd $RUNPATH; lockfile -1 $NUM_FILE.lock; echo $(($MAX_NUM+1)) > $NUM_FILE; rm -f $NUM_FILE.lock"
+		break
+	done
+
+	for machine in $(echo $MACHINES | sed "s/ /\n/g" | uniq); do
+		echo "Stopping $machine"
+		ssh -4 $machine "killall -9 demo_slam; screen -wipe"
+		break
+	done
+
+	for machine in $MACHINES; do
+		ssh -4 $machine "cd $RUNPATH; rm -f $NUM_FILE"
+		break
+	done
+
+fi

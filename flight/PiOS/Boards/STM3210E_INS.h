ab3127a0ea96f136dcabbbecf81bf7b9d587936e
James Cotton <peabody124@gmail.com>
2011-05-18 13:40:39 -0500

    OP-378 Update the INS Makefile for the new file formats

--- flight/PiOS/Boards/STM3210E_INS.h
+++ flight/PiOS/Boards/STM3210E_INS.h
@@ -70,17 +70,13 @@ TIM8  |           |           |           |
 // BOOTLOADER_SETTINGS
 //------------------------
 
-//#define FUNC_ID				1
-//#define HW_VERSION			01
+#define BOOTLOADER_VERSION      0
+#define BOARD_TYPE              0x05  // INS board
+#define BOARD_REVISION          0x01  // Beta version
 
-#define BOOTLOADER_VERSION	0
-#define BOARD_TYPE			0x05  // INS board
-#define BOARD_REVISION		0x01  // Beta version
-//#define HW_VERSION	(BOARD_TYPE << 8) | BOARD_REVISION
-
-#define MEM_SIZE			524288 //512K
+#define MEM_SIZE                524288 //512K
 #define SIZE_OF_DESCRIPTION	(uint8_t) 100
-#define START_OF_USER_CODE	(uint32_t)0x08005000//REMEMBER SET ALSO IN link_stm32f10x_HD_BL.ld
+#define START_OF_USER_CODE	(uint32_t)0x08002000//REMEMBER SET ALSO IN link_STM3210E_INS_memory.ld
 #define SIZE_OF_CODE		(uint32_t) (MEM_SIZE-(START_OF_USER_CODE-0x08000000)-SIZE_OF_DESCRIPTION)
 
 #ifdef STM32F10X_HD

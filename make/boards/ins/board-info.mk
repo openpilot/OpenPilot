c240a927ab74f992a34460712bc00b14d9ef3000
Stacey Sheldon <stac@solidgoldbomb.org>
2011-05-24 23:47:03 -0400

    build: fixup ins board-info.mk file to match new format

--- make/boards/ins/board-info.mk
+++ make/boards/ins/board-info.mk
@@ -9,5 +9,10 @@ BOARD               := STM3210E_INS
 MODEL               := HD
 MODEL_SUFFIX        := _OP
 
-START_OF_BL_CODE    := 0x08000000
-START_OF_FW_CODE    := 0x08002000
+# Note: These must match the values in link_$(BOARD)_memory.ld
+BL_BANK_BASE        := 0x08000000  # Start of bootloader flash
+BL_BANK_SIZE        := 0x00002000  # Should include BD_INFO region
+FW_BANK_BASE        := 0x08002000  # Start of firmware flash
+FW_BANK_SIZE        := 0x0001E000  # Should include FW_DESC_SIZE
+
+FW_DESC_SIZE        := 0x00000064

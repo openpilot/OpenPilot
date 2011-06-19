428d2a63843bf763defc7a33e7c39c531b9c5351
Frederic Goddeeris <frederic@rc-flight.be>
2011-06-19 16:14:38 +0200

    fix: uavobjgenerator did build for all when -pyhtonground was selected
    Conflicts:
    	ground/uavobjgenerator/main.cpp

--- ground/uavobjgenerator/main.cpp
+++ ground/uavobjgenerator/main.cpp
@@ -114,7 +114,7 @@ int main(int argc, char *argv[])
     bool do_wireshark  = (arguments_stringlist.removeAll("-wireshark") > 0);
     bool do_none       = (arguments_stringlist.removeAll("-none") > 0); //
 
-    bool do_all        = ((do_gcs || do_flight || do_java || do_python || do_matlab) == false);
+    bool do_all        = ((do_gcs || do_flight || do_java || do_python || do_pythonground || do_matlab) == false);
     bool do_allObjects = true;
 
     if (arguments_stringlist.length() >= 2) {

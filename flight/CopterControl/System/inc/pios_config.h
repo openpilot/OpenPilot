2b11395a6714c65803795d189402dbde03d71dfb
Mathieu Rondonneau <mathieu.rondonneau@gmail.com>
2011-06-18 17:31:29 -0700

    OP-423 just move stack from systemmod back to heap (200 bytes) that are not needed anymore.

--- flight/CopterControl/System/inc/pios_config.h
+++ flight/CopterControl/System/inc/pios_config.h
@@ -95,7 +95,7 @@
 /* Task stack sizes */
 #define PIOS_ACTUATOR_STACK_SIZE       1020
 #define PIOS_MANUAL_STACK_SIZE          724
-#define PIOS_SYSTEM_STACK_SIZE          560
+#define PIOS_SYSTEM_STACK_SIZE          360
 #define PIOS_STABILIZATION_STACK_SIZE   524
 #define PIOS_TELEM_STACK_SIZE           500
 

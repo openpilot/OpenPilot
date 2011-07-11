38d4e09eab4cb9a9e44ba175dba2f3b33579af98
Cyril Roussillon <cyril.roussillon@laas.fr>
2011-07-11 16:39:23 +0200

    small fixes in montecarlo script

--- data/scripts/montecarlo_ssh.sh
+++ data/scripts/montecarlo_ssh.sh
@@ -20,20 +20,20 @@ MACHINES="\
 	germanium germanium germanium germanium \
 	arsenic arsenic arsenic arsenic \
 	chrome chrome chrome chrome \
-	cobalt cobalt cobalt cobalt \
-	nickel nickel nickel \
 	bore bore bore bore \
 	calcium calcium calcium calcium \
 	gallium gallium gallium gallium \
 	silicium silicium silicium silicium \
-	azote azote azote azote azote \
-	antimoine antimoine antimoine antimoine \
+	azote azote azote azote \
 	jaune jaune jaune jaune \
 	poussin poussin poussin poussin \
 	ocre ocre ocre ocre \
 	safran safran safran safran \
 	blond blond blond blond\
 "
+#	cobalt cobalt cobalt cobalt \
+#	nickel nickel nickel nickel \
+#	antimoine antimoine antimoine antimoine \
 
 RUNPATH=/home/croussil/Libs/jafar/build_release/modules/rtslam
 NUM_FILE=nees.log
@@ -45,7 +45,7 @@ if [[ "$1" -ne "0" ]]; then
 	for machine in $MACHINES; do
 		ssh -4 -t $machine "cd $RUNPATH; screen -S rtslam -d -m data/scripts/montecarlo_loop.sh $NUM_FILE $MAX_NUM"
 		sleep 1
-	done
+	done;
 
 else
 	# STOP
@@ -53,17 +53,16 @@ else
 	for machine in $MACHINES; do
 		ssh -4 $machine "cd $RUNPATH; lockfile -1 $NUM_FILE.lock; echo $(($MAX_NUM+1)) > $NUM_FILE; rm -f $NUM_FILE.lock"
 		break
-	done
+	done;
 
 	for machine in $(echo $MACHINES | sed "s/ /\n/g" | uniq); do
 		echo "Stopping $machine"
-		ssh -4 $machine "killall -9 demo_slam; screen -wipe"
-		break
-	done
+		ssh -4 $machine "killall -9 demo_slam; killall -9 screen; screen -wipe"
+	done;
 
 	for machine in $MACHINES; do
 		ssh -4 $machine "cd $RUNPATH; rm -f $NUM_FILE"
 		break
-	done
+	done;
 
-fi
+fi;

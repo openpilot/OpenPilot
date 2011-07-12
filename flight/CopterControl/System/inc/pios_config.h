15a2b857294df1536c3fee6c30f6ee7b619fec8f
James Cotton <peabody124@gmail.com>
2011-07-12 12:34:40 -0500

    For OpenPilot use the minimal stack size (512 words) for eventdispatcher as this is necessary for object saving.  Creating a new define to override this for CopterControl down to 96.

--- flight/CopterControl/System/inc/pios_config.h
+++ flight/CopterControl/System/inc/pios_config.h
@@ -5,28 +5,28 @@
  * @addtogroup OpenPilotCore OpenPilot Core
  * @{
  *
- * @file       pios_config.h  
+ * @file       pios_config.h
  * @author     The OpenPilot Team, http://www.openpilot.org Copyright (C) 2010.
- * @brief      PiOS configuration header. 
+ * @brief      PiOS configuration header.
  *             Central compile time config for the project.
  *             In particular, pios_config.h is where you define which PiOS libraries
  *             and features are included in the firmware.
  * @see        The GNU Public License (GPL) Version 3
  *
  *****************************************************************************/
-/* 
- * This program is free software; you can redistribute it and/or modify 
- * it under the terms of the GNU General Public License as published by 
- * the Free Software Foundation; either version 3 of the License, or 
+/*
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
  * (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful, but 
- * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY 
- * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License 
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
  * for more details.
- * 
- * You should have received a copy of the GNU General Public License along 
- * with this program; if not, write to the Free Software Foundation, Inc., 
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
  * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
  */
 
@@ -97,7 +97,7 @@
 #define PIOS_SYSTEM_STACK_SIZE          560
 #define PIOS_STABILIZATION_STACK_SIZE   524
 #define PIOS_TELEM_STACK_SIZE           500
-
+#define PIOS_EVENTDISPATCHER_STACK_SIZE 96
 #define IDLE_COUNTS_PER_SEC_AT_NO_LOAD 1995998
 //#define PIOS_QUATERNION_STABILIZATION
 

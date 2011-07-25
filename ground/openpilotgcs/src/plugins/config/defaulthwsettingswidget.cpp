4dc5e7ea651faaface7d8a6f68fff1746760f64e
zedamota <josembarros@hotmail.com>
2011-07-25 14:18:09 +0100

    Fixed included file case. Changed "throttle curve 1/2" to "curve 1/2".

--- ground/openpilotgcs/src/plugins/config/defaulthwsettingswidget.cpp
+++ ground/openpilotgcs/src/plugins/config/defaulthwsettingswidget.cpp
@@ -24,7 +24,7 @@
  * with this program; if not, write to the Free Software Foundation, Inc.,
  * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
  */
-#include "DefaultHwSettingsWidget.h"
+#include "defaulthwsettingswidget.h"
 #include "ui_defaultattitude.h"
 #include <QMutexLocker>
 #include <QErrorMessage>

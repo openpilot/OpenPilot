bac783631873ab6fea716447abde112cc7cbc390
James Cotton <peabody124@gmail.com>
2011-08-04 07:17:36 -0500

    Revert "uavobj: shrink metadata struct to save RAM".  I was premature merging this to next as it changes the on-the-wire representation and thus doesn't allow GCS to set the metadata.
    This reverts commit a641cd79566c00ea9ecbcd0a9b0cd210c469d243.

--- flight/UAVObjects/inc/uavobjectmanager.h
+++ flight/UAVObjects/inc/uavobjectmanager.h
@@ -59,20 +59,16 @@ typedef enum {
  * properties for each object and can be used by multiple modules (e.g. telemetry and logger)
  */
 typedef struct {
-	uint8_t access : 1; /** Defines the access level for the local transactions (readonly and readwrite) */
-	uint8_t gcsAccess : 1; /** Defines the access level for the local GCS transactions (readonly and readwrite), not used in the flight s/w */
-	uint8_t telemetryAcked : 1; /** Defines if an ack is required for the transactions of this object (1:acked, 0:not acked) */
-	uint8_t gcsTelemetryAcked : 1; /** Defines if an ack is required for the transactions of this object (1:acked, 0:not acked) */
-	uint8_t : 4;
-
-	uint8_t telemetryUpdateMode : 2; /** Update mode used by the telemetry module (UAVObjUpdateMode) */
-	uint8_t gcsTelemetryUpdateMode : 2; /** Update mode used by the GCS (UAVObjUpdateMode) */
-	uint8_t loggingUpdateMode : 2; /** Update mode used by the logging module (UAVObjUpdateMode) */
-	uint8_t : 2;
-
-	uint16_t gcsTelemetryUpdatePeriod; /** Update period used by the GCS (only if telemetry mode is PERIODIC) */
-	uint16_t telemetryUpdatePeriod; /** Update period used by the telemetry module (only if telemetry mode is PERIODIC) */
-	uint16_t loggingUpdatePeriod; /** Update period used by the logging module (only if logging mode is PERIODIC) */
+	uint8_t access; /** Defines the access level for the local transactions (readonly and readwrite) */
+	uint8_t gcsAccess; /** Defines the access level for the local GCS transactions (readonly and readwrite), not used in the flight s/w */
+	uint8_t telemetryAcked; /** Defines if an ack is required for the transactions of this object (1:acked, 0:not acked) */
+	uint8_t telemetryUpdateMode; /** Update mode used by the telemetry module (UAVObjUpdateMode) */
+	uint32_t telemetryUpdatePeriod; /** Update period used by the telemetry module (only if telemetry mode is PERIODIC) */
+	uint8_t gcsTelemetryAcked; /** Defines if an ack is required for the transactions of this object (1:acked, 0:not acked) */
+	uint8_t gcsTelemetryUpdateMode; /** Update mode used by the GCS (UAVObjUpdateMode) */
+	uint32_t gcsTelemetryUpdatePeriod; /** Update period used by the GCS (only if telemetry mode is PERIODIC) */
+	uint8_t loggingUpdateMode; /** Update mode used by the logging module (UAVObjUpdateMode) */
+	uint32_t loggingUpdatePeriod; /** Update period used by the logging module (only if logging mode is PERIODIC) */
 } __attribute__((packed)) UAVObjMetadata;
 
 /**

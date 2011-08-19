01a844e0da7a45ac6339e59d37048f9bdb33ba22
Corvus Corax <corvuscorax@cybertrench.com>
2011-08-19 12:11:05 +0200

    Fixed redefinition of configCHECK_FOR_STACK_OVERFLOW in OpenPilot/System/inc/FreeRTOSConfig.h

--- flight/OpenPilot/System/inc/FreeRTOSConfig.h
+++ flight/OpenPilot/System/inc/FreeRTOSConfig.h
@@ -39,7 +39,7 @@
 #define configUSE_RECURSIVE_MUTEXES	1
 #define configUSE_COUNTING_SEMAPHORES	0
 #define configUSE_ALTERNATIVE_API	0
-#define configCHECK_FOR_STACK_OVERFLOW	2
+//#define configCHECK_FOR_STACK_OVERFLOW	2
 #define configQUEUE_REGISTRY_SIZE	10
 
 /* Co-routine definitions. */

952b4e1809cb866f1bf5248e9de01cf04dc7f0bb
James Cotton <peabody124@gmail.com>
2011-08-19 14:35:13 -0500

    Have to speed up the comms rate for gyro to handle the oversampling we are using.

--- flight/INS/pios_board.c
+++ flight/INS/pios_board.c
@@ -487,7 +487,7 @@ static const struct pios_i2c_adapter_cfg pios_i2c_gyro_adapter_cfg = {
 		.I2C_Ack                 = I2C_Ack_Enable,
 		.I2C_AcknowledgedAddress = I2C_AcknowledgedAddress_7bit,
 		.I2C_DutyCycle           = I2C_DutyCycle_2,
-		.I2C_ClockSpeed          = 400000,	/* bits/s */
+		.I2C_ClockSpeed          = 1000000,	/* bits/s */
 	},
 	.transfer_timeout_ms = 50,
 	.scl = {

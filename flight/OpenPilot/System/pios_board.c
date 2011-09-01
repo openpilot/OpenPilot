86ce79af1d1395fd1aec6e36a21926931f5c33cc
Stacey Sheldon <stac@solidgoldbomb.org>
2011-08-31 21:43:57 -0400

    openpilot: fix compile error on OP

--- flight/OpenPilot/System/pios_board.c
+++ flight/OpenPilot/System/pios_board.c
@@ -1168,19 +1168,20 @@ void PIOS_Board_Init(void) {
 		break;
 	case HWSETTINGS_OP_FLEXIPORT_GPS:
 #if defined(PIOS_INCLUDE_GPS)
-	{
-		uint32_t pios_usart_gps_id;
-		if (PIOS_USART_Init(&pios_usart_gps_id, &pios_usart_gps_cfg)) {
-			PIOS_Assert(0);
-		}
-		uint8_t * rx_buffer = (uint8_t *) pvPortMalloc(PIOS_COM_GPS_RX_BUF_LEN);
-		uint8_t * tx_buffer = (uint8_t *) pvPortMalloc(PIOS_COM_GPS_TX_BUF_LEN);
-		PIOS_Assert(rx_buffer);
-		PIOS_Assert(tx_buffer);
-		if (PIOS_COM_Init(&pios_com_gps_id, &pios_usart_com_driver, pios_usart_gps_id,
-				  rx_buffer, PIOS_COM_GPS_RX_BUF_LEN,
-				  tx_buffer, PIOS_COM_GPS_TX_BUF_LEN)) {
-			PIOS_Assert(0);
+		{
+			uint32_t pios_usart_gps_id;
+			if (PIOS_USART_Init(&pios_usart_gps_id, &pios_usart_gps_cfg)) {
+				PIOS_Assert(0);
+			}
+			uint8_t * rx_buffer = (uint8_t *) pvPortMalloc(PIOS_COM_GPS_RX_BUF_LEN);
+			uint8_t * tx_buffer = (uint8_t *) pvPortMalloc(PIOS_COM_GPS_TX_BUF_LEN);
+			PIOS_Assert(rx_buffer);
+			PIOS_Assert(tx_buffer);
+			if (PIOS_COM_Init(&pios_com_gps_id, &pios_usart_com_driver, pios_usart_gps_id,
+						rx_buffer, PIOS_COM_GPS_RX_BUF_LEN,
+						tx_buffer, PIOS_COM_GPS_TX_BUF_LEN)) {
+				PIOS_Assert(0);
+			}
 		}
 #endif	/* PIOS_INCLUDE_GPS */
 		break;

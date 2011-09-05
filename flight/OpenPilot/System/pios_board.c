1e9bcf8426edc3e7fbf4d6657671b3fc6667f9a8
Stacey Sheldon <stac@solidgoldbomb.org>
2011-09-04 23:22:29 -0400

    openpilot: fix merge of unidirectional gps feature branch

--- flight/OpenPilot/System/pios_board.c
+++ flight/OpenPilot/System/pios_board.c
@@ -1176,21 +1176,9 @@ void PIOS_Board_Init(void) {
 			PIOS_Assert(rx_buffer);
 			if (PIOS_COM_Init(&pios_com_gps_id, &pios_usart_com_driver, pios_usart_gps_id,
 						rx_buffer, PIOS_COM_GPS_RX_BUF_LEN,
-						0, 0)) {
+						NULL, 0)) {
 				PIOS_Assert(0);
 			}
-	{
-		uint32_t pios_usart_gps_id;
-		if (PIOS_USART_Init(&pios_usart_gps_id, &pios_usart_gps_cfg)) {
-			PIOS_Assert(0);
-		}
-		uint8_t * rx_buffer = (uint8_t *) pvPortMalloc(PIOS_COM_GPS_RX_BUF_LEN);
-		PIOS_Assert(rx_buffer);
-		if (PIOS_COM_Init(&pios_com_gps_id, &pios_usart_com_driver, pios_usart_gps_id,
-				  rx_buffer, PIOS_COM_GPS_RX_BUF_LEN,
-				  NULL, 0)) {
-			PIOS_Assert(0);
->>>>>>> origin/CorvusCorax_unidirectional-GPS-com
 		}
 #endif	/* PIOS_INCLUDE_GPS */
 		break;

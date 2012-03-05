50b74fa90c1d7c0f026a0ec8fa016d7132c2e7ed
Stacey Sheldon <stac@solidgoldbomb.org>
2012-03-04 20:01:41 -0500

    revo: increase FW_BANK_SIZE to be flash sector aligned
    The F4 CPUs have 16KB x 4, 64KB x 1 and 128KB x 7 sectors
    of flash.  The FW image currently ends in the middle of a
    128KB sector.  FW_BANK is now increased to end on a sector
    boundary.

--- make/boards/revolution/board-info.mk
+++ make/boards/revolution/board-info.mk
@@ -16,7 +16,7 @@ OPENOCD_CONFIG      := stm32f4xx.stlink.cfg
 BL_BANK_BASE        := 0x08000000  # Start of bootloader flash
 BL_BANK_SIZE        := 0x00008000  # Should include BD_INFO region
 FW_BANK_BASE        := 0x08008000  # Start of firmware flash
-FW_BANK_SIZE        := 0x0001E000  # Should include FW_DESC_SIZE
+FW_BANK_SIZE        := 0x00038000  # Should include FW_DESC_SIZE
 
 FW_DESC_SIZE        := 0x00000064
 

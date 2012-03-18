9032284f6680269f6116c5c2ba709cdeb2035f75
Brian Webb <webbbn@gmail.com>
2012-03-18 10:32:26 -0700

    Reverted change to firmware-defs.mk

--- make/firmware-defs.mk
+++ make/firmware-defs.mk
@@ -216,7 +216,7 @@ OOCD_EXE ?= openocd
 OOCD_JTAG_SETUP  = -d0
 # interface and board/target settings (using the OOCD target-library here)
 OOCD_JTAG_SETUP += -s $(TOP)/flight/Project/OpenOCD
-OOCD_JTAG_SETUP += -f flyswatter.cfg -f $(4)
+OOCD_JTAG_SETUP += -f foss-jtag.revb.cfg -f $(4)
 
 # initialize
 OOCD_BOARD_RESET = -c init

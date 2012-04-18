b97b0617aa05d5f59140f29aa3aaa9a351f64392
Corvus Corax <corvuscorax@cybertrench.com>
2012-04-18 23:07:02 +0200

    bugfix, use correct variance parameter on z velocity

--- flight/Libraries/insgps13state.c
+++ flight/Libraries/insgps13state.c
@@ -183,7 +183,7 @@ void INSSetPosVelVar(float PosVar, float VelVar)
 	R[2] = PosVar;
 	R[3] = VelVar;
 	R[4] = VelVar;
-    R[5] = PosVar;  // Don't change vertical velocity, not measured
+	R[5] = VelVar;
 }
 
 void INSSetGyroBias(float gyro_bias[3])

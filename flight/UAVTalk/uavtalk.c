e006ae2b4decedce91544d5791899cc53bd5e10a
James Cotton <peabody124@gmail.com>
2012-07-24 11:07:40 -0500

    Fix a really stupid bug where I ordered two cases together;

--- flight/UAVTalk/uavtalk.c
+++ flight/UAVTalk/uavtalk.c
@@ -610,7 +610,8 @@ static int32_t receiveObject(UAVTalkConnectionData *connection, uint8_t type, ui
 	
 	// Process message type
 	switch (type) {
-		case UAVTALK_TYPE_OBJ | UAVTALK_TYPE_OBJ_TS:
+		case UAVTALK_TYPE_OBJ:
+		case UAVTALK_TYPE_OBJ_TS:
 			// All instances, not allowed for OBJ messages
 			if (obj && (instId != UAVOBJ_ALL_INSTANCES))
 			{
@@ -624,7 +625,8 @@ static int32_t receiveObject(UAVTalkConnectionData *connection, uint8_t type, ui
 				ret = -1;
 			}
 			break;
-		case UAVTALK_TYPE_OBJ_ACK | UAVTALK_TYPE_OBJ_ACK_TS:
+		case UAVTALK_TYPE_OBJ_ACK:
+		case UAVTALK_TYPE_OBJ_ACK_TS:
 			// All instances, not allowed for OBJ_ACK messages
 			if (obj && (instId != UAVOBJ_ALL_INSTANCES))
 			{

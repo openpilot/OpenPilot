9d76c32cf5ba2141e54b8ffb0a466b91d4b60d3f
PT_Dreamer <josemanuelbarros@gmail.com>
2012-07-27 11:45:58 +0100

    GCS-Comment out the updated statements on vehicleconfig.cpp TODO check if this brings other problems. REVERT commit if it does

--- ground/openpilotgcs/src/plugins/config/cfg_vehicletypes/vehicleconfig.cpp
+++ ground/openpilotgcs/src/plugins/config/cfg_vehicletypes/vehicleconfig.cpp
@@ -105,7 +105,7 @@ void VehicleConfig::SetConfigData(GUIConfigDataUnion configData) {
         systemSettingsData.GUIConfigData[i] = configData.UAVObject[i];
 
     systemSettings->setData(systemSettingsData);
-    systemSettings->updated();
+    //systemSettings->updated();
 
     //emit ConfigurationChanged();
 }
@@ -170,7 +170,8 @@ void VehicleConfig::setMixerType(UAVDataObject* mixer, int channel, MixerTypeEle
             if (mixerType >= 0 && mixerType < mixerTypeDescriptions.count())
             {
                 field->setValue(mixerTypeDescriptions[mixerType]);
-                mixer->updated();
+               // mixer->updated();
+                qDebug()<<"updateMixer";
             }
         }
     }
@@ -218,7 +219,7 @@ void VehicleConfig::setMixerVectorValue(UAVDataObject* mixer, int channel, Mixer
 
         if (field) {
             field->setDouble(value, elementName);
-            mixer->updated();
+            //mixer->updated();
         }
     }
 }

1fdc5d50a9e51d625cf70444e4073a64847288b8
James Cotton <peabody124@gmail.com>
2012-08-06 09:04:58 -0500

    AndroidGCS: An ugly system alarms gadget that lists all the alarm values

---
+++ androidgcs/src/org/openpilot/androidgcs/SystemAlarmActivity.java
@@ -0,0 +1,45 @@
+package org.openpilot.androidgcs;
+
+import java.util.List;
+
+import org.openpilot.uavtalk.UAVObject;
+import org.openpilot.uavtalk.UAVObjectField;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class SystemAlarmActivity extends ObjectManagerActivity {
+	/**
+	 * Update the UI whenever the attitude is updated
+	 */
+	protected void objectUpdated(UAVObject obj) {
+		if (obj.getName().compareTo("SystemAlarms") != 0)
+			return;
+
+		TextView alarms = (TextView) findViewById(R.id.system_alarms_status);
+		UAVObjectField a = obj.getField("Alarm");
+		List<String> names = a.getElementNames();
+		String contents = new String();
+		for (int i = 0; i < names.size(); i++) {
+			contents += names.get(i) + " : " + a.getValue(i).toString() + "\n";
+		}
+		alarms.setText(contents);
+	}
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.system_alarms);
+	}	
+
+	@Override
+	void onOPConnected() {
+		super.onOPConnected();
+		
+		// Connect the update method to AttitudeActual
+		UAVObject obj = objMngr.getObject("SystemAlarms");
+		if (obj != null)
+			registerObjectUpdates(obj);
+		objectUpdated(obj);
+	}			
+}

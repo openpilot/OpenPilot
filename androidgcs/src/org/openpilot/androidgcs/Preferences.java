dff1344b6c0c042718a40e44e74a80a7501a0f03
James Cotton <peabody124@gmail.com>
2012-08-06 01:15:29 -0500

    AndroidGCS: Update preferences to the new Fragment API

--- androidgcs/src/org/openpilot/androidgcs/Preferences.java
+++ androidgcs/src/org/openpilot/androidgcs/Preferences.java
@@ -2,11 +2,22 @@ package org.openpilot.androidgcs;
 
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
+import android.preference.PreferenceFragment;
 
 public class Preferences extends PreferenceActivity {
-	  @Override
-	  protected void onCreate(Bundle savedInstanceState) { 
-	    super.onCreate(savedInstanceState);
-	    addPreferencesFromResource(R.xml.preferences); 
-	  }
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		// Display the fragment as the main content.
+        getFragmentManager().beginTransaction().replace(android.R.id.content,
+                new PrefsFragment()).commit();
+	}
+	public static class PrefsFragment extends PreferenceFragment {
+		@Override
+		public void onCreate(Bundle savedInstanceState) {
+			super.onCreate(savedInstanceState);
+			// Load the preferences from an XML resource
+			addPreferencesFromResource(R.xml.preferences);
+		}
+	}
 }

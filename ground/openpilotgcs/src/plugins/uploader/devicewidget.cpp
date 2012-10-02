71e1a14e93ccf247937714cda1c59d0ab0cc017a
James Cotton <peabody124@gmail.com>
2012-10-02 09:37:55 -0500

    GCS Uploader: Add a backward compatibility condition for RM1 using RM2 firmware

--- ground/openpilotgcs/src/plugins/uploader/devicewidget.cpp
+++ ground/openpilotgcs/src/plugins/uploader/devicewidget.cpp
@@ -353,7 +353,9 @@ void deviceWidget::uploadFirmware()
         int board = m_dfu->devices[deviceID].ID;
         int firmwareBoard = ((desc.at(12)&0xff)<<8) + (desc.at(13)&0xff);
         if((board == 0x401 && firmwareBoard == 0x402) ||
-           (board == 0x901 && firmwareBoard == 0x902)) {
+           (board == 0x901 && firmwareBoard == 0x902) || // L3GD20 revo supports Revolution firmware
+           (board == 0x902 && firmwareBoard == 0x903))   // RevoMini1 supporetd by RevoMini2 firmware
+        {
             // These firmwares are designed to be backwards compatible
         } else if (firmwareBoard != board) {
             status("Error: firmware does not match board", STATUSICON_FAIL);

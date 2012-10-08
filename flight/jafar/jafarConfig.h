551065d36f16b25b444fc4a52bef816d1f740562
Corvus Corax <corvuscorax@cybertrench.com>
2012-10-08 10:34:18 +0200

    included rtslam (jafar) subdirectory and Makefile changes to compile SimPosix with rtslam bindings

---
+++ flight/jafar/jafarConfig.h
@@ -0,0 +1,68 @@
+/* jafarConfig.h.in  */
+
+/* boost library */
+#define HAVE_BOOST
+
+/* boost sandbox library */
+#define HAVE_BOOST_SANDBOX
+
+/* lapack library */
+#define HAVE_LAPACK
+
+/* Bayes++ library */
+/* #undef HAVE_BAYESPP */
+
+/* openCV library */
+#define HAVE_OPENCV
+
+/* opengl library */
+#define HAVE_OPENGL
+
+/* GDHE */
+//#define HAVE_GDHE
+
+/* posterLib */
+/* #undef HAVE_POSTERLIB */
+
+/* CGAL library */
+/* #undef HAVE_CGAL */
+
+/* IEEE1394 support */
+/* #undef HAVE_IEEE1394 */
+
+/* v4l support */
+/* #undef HAVE_V4L */
+
+/* Quicktime support */
+/* #undef HAVE_QUICKTIME */
+
+/* PNL library */
+/* #undef HAVE_PNL */
+
+/* Calife Camera Library */
+/* #undef HAVE_CALIFECAMERA */
+
+/* OpenSceneGraph */
+//#define HAVE_OSG
+
+/* T3D */
+/* #undef HAVE_T3D */
+
+/* Stereopixel */
+/* #undef HAVE_STEREOPIXEL */
+
+/* Viam */
+/* #undef HAVE_VIAM */
+
+/* Ueye */
+/* #undef HAVE_UEYE */
+
+/* MTI */
+/* #undef HAVE_MTI */
+
+/* SIFT */
+/* #undef HAVE_SIFT */
+
+/* FLANN */
+/* #undef HAVE_FLANN */
+

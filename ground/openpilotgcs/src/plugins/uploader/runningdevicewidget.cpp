5c141d458d88fdcbc6e8c41ad1376ff9a0473447
Oleg Semyonov <os@openpilot.org>
2012-10-15 23:33:53 +0300

    Temporary fix for build label (until better system is implemented)

--- ground/openpilotgcs/src/plugins/uploader/runningdevicewidget.cpp
+++ ground/openpilotgcs/src/plugins/uploader/runningdevicewidget.cpp
@@ -103,7 +103,7 @@ void runningDeviceWidget::populate()
     deviceDescriptorStruct devDesc;
     if(UAVObjectUtilManager::descriptionToStructure(description,devDesc))
     {
-        if(devDesc.gitTag.startsWith("release",Qt::CaseInsensitive))
+        if (devDesc.gitTag.compare("master") == 0)
         {
             myDevice->lblFWTag->setText(QString("Firmware tag: ")+devDesc.gitTag);
             QPixmap pix = QPixmap(QString(":uploader/images/application-certificate.svg"));

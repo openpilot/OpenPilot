0e55f3c74dda329f3723f28deca2a437340ed1e0
Fredrik Arvidsson <fredrik@arvidssons.org>
2012-11-24 14:55:46 +0100

    OP-723 Changed default locale to English. Updated default configuration files to have <OverrideLanguage> tag set to en_US.

--- ground/openpilotgcs/src/plugins/coreplugin/dialogs/importsettings.cpp
+++ ground/openpilotgcs/src/plugins/coreplugin/dialogs/importsettings.cpp
@@ -10,47 +10,47 @@ importSettings::importSettings(QWidget *parent) :
     ui(new Ui::importSettings)
 {
     ui->setupUi(this);
-    connect(ui->cbConfigs,SIGNAL(currentIndexChanged(int)),this,SLOT(updateDetails(int)));
-    connect(ui->btnLoad,SIGNAL(clicked()),this,SLOT(accept()));
-    QTimer::singleShot(500,this,SLOT(repaint()));
+    connect(ui->cbConfigs, SIGNAL(currentIndexChanged(int)), this, SLOT(updateDetails(int)));
+    connect(ui->btnLoad, SIGNAL(clicked()), this, SLOT(accept()));
+    QTimer::singleShot(500, this, SLOT(repaint()));
 }
+
 void importSettings::loadFiles(QString path)
 {
     QDir myDir(path);
     QStringList filters;
     filters << "*.xml";
     QStringList list = myDir.entryList(filters,QDir::Files);
-    int x=0;
-    foreach(QString fileStr, list)
-    {
-        fileInfo * info=new fileInfo;
-        QSettings settings(path+QDir::separator()+fileStr, XmlConfig::XmlSettingsFormat);
+    int x = 0;
+    foreach(QString fileStr, list) {
+        fileInfo *info = new fileInfo;
+        QSettings settings(path+QDir::separator() + fileStr, XmlConfig::XmlSettingsFormat);
         settings.beginGroup("General");
-        info->description=settings.value("Description","None").toString();
-        info->details=settings.value("Details","None").toString();
+        info->description = settings.value("Description", "None").toString();
+        info->details = settings.value("Details", "None").toString();
         settings.endGroup();
-        info->file=path+QDir::separator()+fileStr;
+        info->file = path + QDir::separator() + fileStr;
         configList.insert(x,info);
-        ui->cbConfigs->addItem(info->description,x);
+        ui->cbConfigs->addItem(info->description, x);
         ++x;
     }
 }
 
 void importSettings::updateDetails(int index)
 {
-    fileInfo * info=configList.value(ui->cbConfigs->itemData(index).toInt());
+    fileInfo *info = configList.value(ui->cbConfigs->itemData(index).toInt());
     ui->lblDetails->setText(info->details);
 }
+
 QString importSettings::choosenConfig()
 {
-    fileInfo * info=configList.value(ui->cbConfigs->itemData(ui->cbConfigs->currentIndex()).toInt());
+    fileInfo *info = configList.value(ui->cbConfigs->itemData(ui->cbConfigs->currentIndex()).toInt());
     return info->file;
 }
 
 importSettings::~importSettings()
 {
-    foreach(fileInfo * info,configList.values())
-    {
+    foreach(fileInfo * info,configList.values()) {
         delete info;
     }
     delete ui;

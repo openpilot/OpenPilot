776ec4f3ea20263a046192f1628b7845fccaca96
Oleg Semyonov <os@openpilot.org>
2013-02-24 04:24:25 +0200

    ut_logfs: do not pollute the source tree with temp files

--- flight/tests/logfs/pios_flash_ut_priv.h
+++ flight/tests/logfs/pios_flash_ut_priv.h
@@ -8,3 +8,7 @@ struct pios_flash_ut_cfg {
 int32_t PIOS_Flash_UT_Init(uintptr_t * flash_id, const struct pios_flash_ut_cfg * cfg);
 
 extern const struct pios_flash_driver pios_ut_flash_driver;
+
+#if !defined(FLASH_IMAGE_FILE)
+#define FLASH_IMAGE_FILE "theflash.bin"
+#endif

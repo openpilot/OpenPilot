441e9e7b8441be30a6d82cd1e2cf4ce757472d73
Stacey Sheldon <stac@solidgoldbomb.org>
2013-03-17 00:29:37 -0400

    f4 usb: always init HID and CDC devices
    This is required so that we can always respond to HID and
    CDC requests via the control endpoint (ep0).  The F1/F3
    implementation gets away with disabling these interfaces
    because all of the ep0 handling is in the usbhook code
    rather than (properly) split out into class-specific
    drivers like it is done on the F4 implementation.
    
    Conflicts:
    	flight/targets/DiscoveryF4/System/pios_board.c
    	flight/targets/FlyingF4/System/pios_board.c
    	flight/targets/Freedom/System/pios_board.c
    	flight/targets/Quanton/System/pios_board.c

--- flight/targets/boards/revolution/firmware/pios_board.c
+++ flight/targets/boards/revolution/firmware/pios_board.c
@@ -453,6 +453,15 @@ void PIOS_Board_Init(void) {
 		/* Force VCP port function to disabled if we haven't advertised VCP in our USB descriptor */
 		hwsettings_usb_vcpport = HWSETTINGS_USB_VCPPORT_DISABLED;
 	}
+	uint32_t pios_usb_cdc_id;
+    if (PIOS_USB_CDC_Init(&pios_usb_cdc_id, &pios_usb_cdc_cfg, pios_usb_id)) {
+        PIOS_Assert(0);
+    }
+
+    uint32_t pios_usb_hid_id;
+    if (PIOS_USB_HID_Init(&pios_usb_hid_id, &pios_usb_hid_cfg, pios_usb_id)) {
+        PIOS_Assert(0);
+    }
 
 	switch (hwsettings_usb_vcpport) {
 	case HWSETTINGS_USB_VCPPORT_DISABLED:
@@ -460,10 +469,6 @@ void PIOS_Board_Init(void) {
 	case HWSETTINGS_USB_VCPPORT_USBTELEMETRY:
 #if defined(PIOS_INCLUDE_COM)
 		{
-			uint32_t pios_usb_cdc_id;
-			if (PIOS_USB_CDC_Init(&pios_usb_cdc_id, &pios_usb_cdc_cfg, pios_usb_id)) {
-				PIOS_Assert(0);
-			}
 			uint8_t * rx_buffer = (uint8_t *) pvPortMalloc(PIOS_COM_TELEM_USB_RX_BUF_LEN);
 			uint8_t * tx_buffer = (uint8_t *) pvPortMalloc(PIOS_COM_TELEM_USB_TX_BUF_LEN);
 			PIOS_Assert(rx_buffer);
@@ -479,10 +484,7 @@ void PIOS_Board_Init(void) {
 	case HWSETTINGS_USB_VCPPORT_COMBRIDGE:
 #if defined(PIOS_INCLUDE_COM)
 		{
-			uint32_t pios_usb_cdc_id;
-			if (PIOS_USB_CDC_Init(&pios_usb_cdc_id, &pios_usb_cdc_cfg, pios_usb_id)) {
-				PIOS_Assert(0);
-			}
+
 			uint8_t * rx_buffer = (uint8_t *) pvPortMalloc(PIOS_COM_BRIDGE_RX_BUF_LEN);
 			uint8_t * tx_buffer = (uint8_t *) pvPortMalloc(PIOS_COM_BRIDGE_TX_BUF_LEN);
 			PIOS_Assert(rx_buffer);
@@ -499,10 +501,6 @@ void PIOS_Board_Init(void) {
 #if defined(PIOS_INCLUDE_COM)
 #if defined(PIOS_INCLUDE_DEBUG_CONSOLE)
 		{
-			uint32_t pios_usb_cdc_id;
-			if (PIOS_USB_CDC_Init(&pios_usb_cdc_id, &pios_usb_cdc_cfg, pios_usb_id)) {
-				PIOS_Assert(0);
-			}
 			uint8_t * tx_buffer = (uint8_t *) pvPortMalloc(PIOS_COM_DEBUGCONSOLE_TX_BUF_LEN);
 			PIOS_Assert(tx_buffer);
 			if (PIOS_COM_Init(&pios_com_debug_id, &pios_usb_cdc_com_driver, pios_usb_cdc_id,
@@ -534,10 +532,6 @@ void PIOS_Board_Init(void) {
 	case HWSETTINGS_USB_HIDPORT_USBTELEMETRY:
 #if defined(PIOS_INCLUDE_COM)
 		{
-			uint32_t pios_usb_hid_id;
-			if (PIOS_USB_HID_Init(&pios_usb_hid_id, &pios_usb_hid_cfg, pios_usb_id)) {
-				PIOS_Assert(0);
-			}
 			uint8_t * rx_buffer = (uint8_t *) pvPortMalloc(PIOS_COM_TELEM_USB_RX_BUF_LEN);
 			uint8_t * tx_buffer = (uint8_t *) pvPortMalloc(PIOS_COM_TELEM_USB_TX_BUF_LEN);
 			PIOS_Assert(rx_buffer);

404ba00f701b2021b95e88ba279c121e2ea6269b
Oleg Semyonov <os@openpilot.org>
2013-03-17 14:36:45 +0200

    Makefile: rework Revolution Makefile to use new make system
    Conflicts:
    	flight/targets/Revolution/Makefile

--- flight/targets/Revolution/System/inc/pios_config.h
+++ flight/targets/Revolution/System/inc/pios_config.h
@@ -39,95 +39,109 @@
 /* #define PIOS_INCLUDE_DEBUG_CONSOLE */
 /* #define DEBUG_LEVEL 0 */
 
+/* PIOS system functions */
 #define PIOS_INCLUDE_FREERTOS
 #define PIOS_INCLUDE_DELAY
 #define PIOS_INCLUDE_INITCALL
 #define PIOS_INCLUDE_SYS
 
+/* PIOS hardware peripherals */
 #define PIOS_INCLUDE_IRQ
 #define PIOS_INCLUDE_RTC
 #define PIOS_INCLUDE_TIM
 #define PIOS_INCLUDE_USART
 #define PIOS_INCLUDE_ADC
-/* #define PIOS_INCLUDE_I2C */
+#define PIOS_INCLUDE_I2C
 #define PIOS_INCLUDE_SPI
-#define PIOS_INCLUDE_GPIO
+/* #define PIOS_INCLUDE_GPIO */
 #define PIOS_INCLUDE_EXTI
 #define PIOS_INCLUDE_WDG
+
+/* PIOS USB functions */
 #define PIOS_INCLUDE_USB
 #define PIOS_INCLUDE_USB_HID
-#define PIOS_INCLUDE_USB_CDC
-#define PIOS_INCLUDE_USB_RCTX
+/* #define PIOS_INCLUDE_USB_CDC */
+/* #define PIOS_INCLUDE_USB_RCTX */
 
-#define PIOS_INCLUDE_ADXL345
-/* #define PIOS_INCLUDE_BMA180 */
-/* #define PIOS_INCLUDE_L3GD20 */
+/* PIOS sensor interfaces */
+/* #define PIOS_INCLUDE_ADXL345 */
+#define PIOS_INCLUDE_BMA180
+#define PIOS_INCLUDE_L3GD20
 #define PIOS_INCLUDE_MPU6000
 #define PIOS_MPU6000_ACCEL
 /* #define PIOS_INCLUDE_HMC5843 */
-/* #define PIOS_INCLUDE_HMC5883 */
-/* #define PIOS_HMC5883_HAS_GPIOS */
+#define PIOS_INCLUDE_HMC5883
+#define PIOS_HMC5883_HAS_GPIOS
 /* #define PIOS_INCLUDE_BMP085 */
-/* #define PIOS_INCLUDE_MS5611 */
-/* #define PIOS_INCLUDE_MPXV */
-/* #define PIOS_INCLUDE_ETASV3 */
+#define PIOS_INCLUDE_MS5611
+#define PIOS_INCLUDE_MPXV
+#define PIOS_INCLUDE_ETASV3
 /* #define PIOS_INCLUDE_HCSR04 */
 
+/* PIOS receiver drivers */
 #define PIOS_INCLUDE_PWM
 #define PIOS_INCLUDE_PPM
 #define PIOS_INCLUDE_DSM
 #define PIOS_INCLUDE_SBUS
 #define PIOS_INCLUDE_GCSRCVR
 
+/* PIOS common peripherals */
 #define PIOS_INCLUDE_LED
 #define PIOS_INCLUDE_IAP
 #define PIOS_INCLUDE_SERVO
 /* #define PIOS_INCLUDE_I2C_ESC */
-/* #define PIOS_INCLUDE_OVERO */
+#define PIOS_INCLUDE_OVERO
+#define PIOS_OVERO_SPI
 /* #define PIOS_INCLUDE_SDCARD */
 #define PIOS_INCLUDE_FLASH
 #define PIOS_INCLUDE_FLASH_SECTOR_SETTINGS
-/* #define FLASH_FREERTOS */
+#define FLASH_FREERTOS
 /* #define PIOS_INCLUDE_FLASH_EEPROM */
 /* #define PIOS_INCLUDE_RFM22B */
 /* #define PIOS_INCLUDE_PACKET_HANDLER */
 /* #define PIOS_INCLUDE_VIDEO */
 /* #define PIOS_INCLUDE_WAVE */
-#define PIOS_INCLUDE_BL_HELPER
-/* #define PIOS_INCLUDE_BL_HELPER_WRITE_SUPPORT */
 /* #define PIOS_INCLUDE_UDP */
 
+/* PIOS abstract receiver interface */
 #define PIOS_INCLUDE_RCVR
+
+/* PIOS abstract comms interface with options */
 #define PIOS_INCLUDE_COM
 /* #define PIOS_INCLUDE_COM_MSG */
-#define PIOS_INCLUDE_TELEMETRY_RF
-/* #define PIOS_INCLUDE_COM_TELEM */
-/* #define PIOS_INCLUDE_COM_FLEXI */
-/* #define PIOS_TELEM_PRIORITY_QUEUE */
+/* #define PIOS_INCLUDE_TELEMETRY_RF */
+#define PIOS_INCLUDE_COM_TELEM
+#define PIOS_INCLUDE_COM_FLEXI
+#define PIOS_INCLUDE_COM_AUX
+#define PIOS_TELEM_PRIORITY_QUEUE
 #define PIOS_INCLUDE_GPS
-#define PIOS_GPS_MINIMAL
+/* #define PIOS_GPS_MINIMAL */
 #define PIOS_INCLUDE_GPS_NMEA_PARSER
 #define PIOS_INCLUDE_GPS_UBX_PARSER
-/* #define PIOS_GPS_SETS_HOMELOCATION */
+#define PIOS_GPS_SETS_HOMELOCATION
+
+/* PIOS bootloader helper */
+#define PIOS_INCLUDE_BL_HELPER
+/* #define PIOS_INCLUDE_BL_HELPER_WRITE_SUPPORT */
 
 /* Performance counters */
-#define IDLE_COUNTS_PER_SEC_AT_NO_LOAD	1995998
+#define IDLE_COUNTS_PER_SEC_AT_NO_LOAD	8379692
 
 /* Alarm Thresholds */
-#define HEAP_LIMIT_WARNING		220
-#define HEAP_LIMIT_CRITICAL		40
-#define IRQSTACK_LIMIT_WARNING		100
-#define IRQSTACK_LIMIT_CRITICAL		60
-#define CPULOAD_LIMIT_WARNING		85
+#define HEAP_LIMIT_WARNING		1000
+#define HEAP_LIMIT_CRITICAL		500
+#define IRQSTACK_LIMIT_WARNING		150
+#define IRQSTACK_LIMIT_CRITICAL		80
+#define CPULOAD_LIMIT_WARNING		80
 #define CPULOAD_LIMIT_CRITICAL		95
 
 /* Task stack sizes */
-#define PIOS_ACTUATOR_STACK_SIZE	1020
-#define PIOS_MANUAL_STACK_SIZE		800
-#define PIOS_SYSTEM_STACK_SIZE		660
-#define PIOS_STABILIZATION_STACK_SIZE	524
-#define PIOS_TELEM_STACK_SIZE		500
-#define PIOS_EVENTDISPATCHER_STACK_SIZE	130
+/* #define PIOS_ACTUATOR_STACK_SIZE	1020 */
+/* #define PIOS_MANUAL_STACK_SIZE		800 */
+/* #define PIOS_SYSTEM_STACK_SIZE		660 */
+/* #define PIOS_STABILIZATION_STACK_SIZE	524 */
+/* #define PIOS_TELEM_STACK_SIZE		500 */
+/* #define PIOS_EVENTDISPATCHER_STACK_SIZE	130 */
 
 /* Stabilization options */
 /* #define PIOS_QUATERNION_STABILIZATION */
@@ -135,7 +149,15 @@
 /* This can't be too high to stop eventdispatcher thread overflowing */
 #define PIOS_EVENTDISAPTCHER_QUEUE	10
 
+/* Revolution series */
+#define REVOLUTION
+
+/* OBSOLETE */
+#define PIOS_INCLUDE_COM_AUXSBUS
+#define PIOS_FLASH_ON_ACCEL /* true for second revo */
+
 #endif /* PIOS_CONFIG_H */
+
 /**
  * @}
  * @}

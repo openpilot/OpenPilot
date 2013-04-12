fdd0ed39e0fccc50205951dd7044dd77916362c3
Oleg Semyonov <os@openpilot.org>
2013-04-13 00:25:18 +0300

    OP-896: remove old board configs (ESC, INS, OpenPilot)

--- flight/PiOS/Boards/pios_board.h
+++ flight/PiOS/Boards/pios_board.h
@@ -1,24 +1,20 @@
-#ifndef PIOS_BOARD_H_
-#define PIOS_BOARD_H_
+#ifndef PIOS_BOARD_H
+#define PIOS_BOARD_H
 
-#ifdef USE_STM3210E_OP
-#include "STM3210E_OP.h"
+#if USE_STM32103CB_CC_Rev1
+#include "STM32103CB_CC_Rev1.h"
 #elif USE_STM32103CB_OPLINKMINI
 #include "STM32103CB_OPLinkMini_Rev1.h"
-#elif USE_STM32103CB_CC_Rev1
-#include "STM32103CB_CC_Rev1.h"
-#elif USE_STM32F2xx_INS
-#include "STM32F2xx_INS.h"
-#elif USE_STM32F4xx_OP
-#include "STM32F4xx_Revolution.h"
-#elif USE_STM32F4xx_OSD
-#include "STM32F4xx_OSD.h"
 #elif USE_STM32F4xx_RM
 #include "STM32F4xx_RevoMini.h"
+#elif USE_STM32F4xx_OSD
+#include "STM32F4xx_OSD.h"
+#elif USE_STM32F4xx_OP
+#include "STM32F4xx_Revolution.h"
 #elif USE_SIM_POSIX
 #include "sim_posix.h"
 #else
 #error Board definition has not been provided.
 #endif
 
-#endif /* PIOS_BOARD_H_ */
+#endif /* PIOS_BOARD_H */

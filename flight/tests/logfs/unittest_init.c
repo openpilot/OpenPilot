4f9ce609306d2c7a8bc8b40473177781a241e53a
Stacey Sheldon <stac@solidgoldbomb.org>
2013-04-18 23:32:52 -0400

    ut logfs: add support for testing a 2nd partition

--- flight/tests/logfs/unittest_init.c
+++ flight/tests/logfs/unittest_init.c
@@ -7,19 +7,30 @@
 
 
 const struct pios_flash_ut_cfg flash_config = {
-	.size_of_flash  = 0x00200000,
+	.size_of_flash  = 0x00300000,
 	.size_of_sector = 0x00010000,
 };
 
 #include "pios_flashfs_logfs_priv.h"
 
-const struct flashfs_logfs_cfg flashfs_config = {
+const struct flashfs_logfs_cfg flashfs_config_partition_a = {
 	.fs_magic      = 0x89abceef,
-	.total_fs_size = 0x00200000, /* 2M bytes (32 sectors = entire chip) */
+	.total_fs_size = 0x00200000, /* 2M bytes (32 sectors) */
 	.arena_size    = 0x00010000, /* 256 * slot size */
 	.slot_size     = 0x00000100, /* 256 bytes */
 
 	.start_offset  = 0,	     /* start at the beginning of the chip */
+	.sector_size   = 0x00010000, /* 64K bytes */
+	.page_size     = 0x00000100, /* 256 bytes */
+};
+
+const struct flashfs_logfs_cfg flashfs_config_partition_b = {
+	.fs_magic      = 0x89abceef,
+	.total_fs_size = 0x00100000, /* 1M bytes (16 sectors) */
+	.arena_size    = 0x00010000, /* 64 * slot size */
+	.slot_size     = 0x00000400, /* 256 bytes */
+
+	.start_offset  = 0x00200000, /* start after partition a */
 	.sector_size   = 0x00010000, /* 64K bytes */
 	.page_size     = 0x00000100, /* 256 bytes */
 };

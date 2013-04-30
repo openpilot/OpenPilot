1eef6a7f066198cdc7c3dcf84b930ff6d599bed8
Alessio Morale <alessiomorale@gmail.com>
2013-04-30 18:26:25 +0200

    OP-917 Extended the program flash size to 3 128k sectors for F4 targets
    +review OPReview-456

--- flight/targets/boards/revoproto/board-info.mk
+++ flight/targets/boards/revoproto/board-info.mk
@@ -1,6 +1,6 @@
 BOARD_TYPE          := 0x09
 BOARD_REVISION      := 0x02
-BOOTLOADER_VERSION  := 0x04
+BOOTLOADER_VERSION  := 0x05
 HW_TYPE             := 0x00
 
 MCU                 := cortex-m4
@@ -19,7 +19,7 @@ BL_BANK_SIZE        := 0x00008000  # Should include BD_INFO region
 # Leave the remaining 16KB and 64KB sectors for other uses
 
 FW_BANK_BASE        := 0x08020000  # Start of firmware flash
-FW_BANK_SIZE        := 0x00040000  # Should include FW_DESC_SIZE
+FW_BANK_SIZE        := 0x00060000  # Should include FW_DESC_SIZE
 
 FW_DESC_SIZE        := 0x00000064
 

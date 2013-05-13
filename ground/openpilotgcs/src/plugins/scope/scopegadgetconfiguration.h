c4adede7ac83018f215963adbe75160ff6d546bf
Fredrik Arvidsson <fredrik@arvidssons.org>
2013-05-13 21:24:19 +0200

    OP-952 Adds settings per scope-plot if to draw the plot anti-aliased. Default setting is true. Adds a fix to not render the plots when they are not visible. Saves CPU.

--- ground/openpilotgcs/src/plugins/scope/scopegadgetconfiguration.h
+++ ground/openpilotgcs/src/plugins/scope/scopegadgetconfiguration.h
@@ -35,66 +35,117 @@
 
 using namespace Core;
 
-struct PlotCurveConfiguration
-{
+struct PlotCurveConfiguration {
     QString uavObject;
     QString uavField;
-    int yScalePower; //This is the power to which each value must be raised
-    QRgb color;
-    int yMeanSamples;
+    int     yScalePower; // This is the power to which each value must be raised
+    QRgb    color;
+    int     yMeanSamples;
     QString mathFunction;
-    double yMinimum;
-    double yMaximum;
+    double  yMinimum;
+    double  yMaximum;
+    bool    drawAntialiased;
 };
 
-class ScopeGadgetConfiguration : public IUAVGadgetConfiguration
-{
+class ScopeGadgetConfiguration : public IUAVGadgetConfiguration {
     Q_OBJECT
 public:
-    explicit ScopeGadgetConfiguration(QString classId, QSettings* qSettings = 0, QObject *parent = 0);
+    explicit ScopeGadgetConfiguration(QString classId, QSettings *qSettings = 0, QObject *parent = 0);
 
     ~ScopeGadgetConfiguration();
 
-    //configuration setter functions
-    void setPlotType(int value){m_plotType = value;}
-    void setMathFunctionType(int value){m_mathFunctionType = value;}
-    void setDataSize(int value){m_dataSize = value;}
-    void setRefreashInterval(int value){m_refreshInterval = value;}
-    void addPlotCurveConfig(PlotCurveConfiguration* value){m_PlotCurveConfigs.append(value);}
-    void replacePlotCurveConfig(QList<PlotCurveConfiguration*> m_PlotCurveConfigs);
+    // configuration setter functions
+    void setPlotType(int value)
+    {
+        m_plotType = value;
+    }
+    void setMathFunctionType(int value)
+    {
+        m_mathFunctionType = value;
+    }
+    void setDataSize(int value)
+    {
+        m_dataSize = value;
+    }
+    void setRefreashInterval(int value)
+    {
+        m_refreshInterval = value;
+    }
+    void addPlotCurveConfig(PlotCurveConfiguration *value)
+    {
+        m_plotCurveConfigs.append(value);
+    }
+    void replacePlotCurveConfig(QList<PlotCurveConfiguration *> m_plotCurveConfigs);
 
 
-    //configurations getter functions
-    int plotType(){return m_plotType;}
-    int mathFunctionType(){return m_mathFunctionType;}
-    int dataSize(){return m_dataSize;}
-    int refreshInterval(){return m_refreshInterval;}
-    QList<PlotCurveConfiguration*> plotCurveConfigs(){return m_PlotCurveConfigs;}
+    // Configurations getter functions
+    int plotType()
+    {
+        return m_plotType;
+    }
+    int mathFunctionType()
+    {
+        return m_mathFunctionType;
+    }
+    int dataSize()
+    {
+        return m_dataSize;
+    }
+    int refreshInterval()
+    {
+        return m_refreshInterval;
+    }
+    QList<PlotCurveConfiguration *> plotCurveConfigs()
+    {
+        return m_plotCurveConfigs;
+    }
 
-    void saveConfig(QSettings* settings) const; //THIS SEEMS TO BE UNUSED
+    void saveConfig(QSettings *settings) const; // THIS SEEMS TO BE UNUSED
     IUAVGadgetConfiguration *clone();
 
-    bool getLoggingEnabled(){return m_LoggingEnabled;};
-    bool getLoggingNewFileOnConnect(){return m_LoggingNewFileOnConnect;};
-    QString getLoggingPath(){return m_LoggingPath;};
-    void setLoggingEnabled(bool value){m_LoggingEnabled=value;};
-    void setLoggingNewFileOnConnect(bool value){m_LoggingNewFileOnConnect=value;};
-    void setLoggingPath(QString value){m_LoggingPath=value;};
+    bool getLoggingEnabled()
+    {
+        return m_loggingEnabled;
+    }
+    bool getLoggingNewFileOnConnect()
+    {
+        return m_loggingNewFileOnConnect;
+    }
+    QString getLoggingPath()
+    {
+        return m_loggingPath;
+    }
+    void setLoggingEnabled(bool value)
+    {
+        m_loggingEnabled = value;
+    }
+    void setLoggingNewFileOnConnect(bool value)
+    {
+        m_loggingNewFileOnConnect = value;
+    }
+    void setLoggingPath(QString value)
+    {
+        m_loggingPath = value;
+    }
 
 private:
 
-    static const uint m_configurationStreamVersion = 1000;//Increment this if the stream format is not compatible with previous versions. This would cause existing configs to be discarded.
-    int m_plotType; //The type of the plot
-    int m_dataSize; //The size of the data buffer to render in the curve plot
-    int m_refreshInterval; //The interval to replot the curve widget. The data buffer is refresh as the data comes in.
-    int m_mathFunctionType; //The type of math function to be used in the scope analysis
-    QList<PlotCurveConfiguration*> m_PlotCurveConfigs;
+    // Increment this if the stream format is not compatible with previous versions. This would cause existing configs to be discarded.
+    static const uint m_configurationStreamVersion = 1000;
+    // The type of the plot
+    int m_plotType;
+    // The size of the data buffer to render in the curve plot
+    int m_dataSize;
+    // The interval to replot the curve widget. The data buffer is refresh as the data comes in.
+    int m_refreshInterval;
+    // The type of math function to be used in the scope analysis
+    int m_mathFunctionType;
+    QList<PlotCurveConfiguration *> m_plotCurveConfigs;
 
     void clearPlotData();
-    bool m_LoggingEnabled;
-    bool m_LoggingNewFileOnConnect;
-    QString m_LoggingPath;
-
+    bool m_loggingEnabled;
+    bool m_loggingNewFileOnConnect;
+    QString m_loggingPath;
 };
 
 #endif // SCOPEGADGETCONFIGURATION_H

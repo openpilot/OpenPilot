6ba15ebc53b8115d5229409617712adad7c3af66
Alessio Morale <alessiomorale@gmail.com>
2013-05-15 23:18:50 +0200

    OP-954 Fixes from review: - CopterControl target fix - tabs in Nakefiles - Removed duplicate stale settings and clean empty lines in osd Makefile +review OPReview-470

--- flight/targets/boards/oplinkmini/board-info.mk
+++ flight/targets/boards/oplinkmini/board-info.mk
@@ -14,13 +14,13 @@ OPENOCD_CONFIG      := stm32f1x.stlink.cfg
 
 # Flash memory map for OPLM:
 # Sector	start			size	use
-# 0			0x0800 0000		1k		BL
-# 1			0x0800 0400		1k		BL
-# ..								..
+# 0		0x0800 0000		1k		BL
+# 1		0x0800 0400		1k		BL
+# ..		..
 # 10		0x0800 2C00		1k		BL
 # 11		0x0800 3000		1k		FW
 # 12		0x0800 1000		1k		FW
-# ..								..
+# ..		..
 # 125 		0x0801 F400		1k		FW
 # 126 		0x0801 F800		1k		EE
 # 127 		0x0801 FC00		1k		EE

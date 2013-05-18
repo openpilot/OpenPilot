2e458b565290add385289b11a6a9474ce6449dbd
Alessio Morale <alessiomorale@gmail.com>
2013-05-18 20:53:28 +0200

    OP-901 USB OTG/Device Drivers are not targeted for f103 (they are targeted at f105)  so move again to F4 library folder instead of common

--- flight/pios/stm32f4xx/library.mk
+++ flight/pios/stm32f4xx/library.mk
@@ -38,13 +38,13 @@ SRC			+= $(sort $(wildcard $(PERIPHLIB)/src/*.c))
 EXTRAINCDIRS		+= $(PERIPHLIB)/inc
 
 # ST USB OTG library
-USBOTGLIB		=  $(PIOSCOMMON)/libraries/STM32_USB_OTG_Driver
+USBOTGLIB		=  $(PIOS_DEVLIB)/libraries/STM32_USB_OTG_Driver
 USBOTGLIB_SRC		=  usb_core.c usb_dcd.c usb_dcd_int.c
 SRC			+= $(addprefix $(USBOTGLIB)/src/,$(USBOTGLIB_SRC))
 EXTRAINCDIRS		+= $(USBOTGLIB)/inc
 
 # ST USB Device library
-USBDEVLIB		=  $(PIOSCOMMON)/libraries/STM32_USB_Device_Library
+USBDEVLIB		=  $(PIOS_DEVLIB)/libraries/STM32_USB_Device_Library
 SRC			+= $(sort $(wildcard $(USBDEVLIB)/Core/src/*.c))
 EXTRAINCDIRS		+= $(USBDEVLIB)/Core/inc
 

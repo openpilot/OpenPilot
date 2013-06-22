4bf934ae696ee38573617029d9409fc53c2e1f27
Corvus Corax <corvuscorax@cybertrench.com>
2013-06-22 23:15:10 +0200

    prevent positionSensor updates if GPS lock is low quality. Sometimes bad updates are worse than no updates.

--- flight/modules/GPS/GPS.c
+++ flight/modules/GPS/GPS.c
@@ -277,9 +277,6 @@ static void gpsTask(__attribute__((unused)) void *parameters)
             } else if ((gpspositionsensor.Status == GPSPOSITIONSENSOR_STATUS_FIX3D) &&
                        (gpspositionsensor.Latitude != 0 || gpspositionsensor.Longitude != 0)) {
                 AlarmsSet(SYSTEMALARMS_ALARM_GPS, SYSTEMALARMS_ALARM_WARNING);
-#ifdef PIOS_GPS_SETS_POSITIONSENSOR
-                setPositionSensor(&gpspositionsensor);
-#endif
             } else {
                 AlarmsSet(SYSTEMALARMS_ALARM_GPS, SYSTEMALARMS_ALARM_CRITICAL);
             }

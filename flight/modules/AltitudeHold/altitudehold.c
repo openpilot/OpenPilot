4c580a4eca09341c2b42cbaa9dbf1e4cb133eaaa
Werner Backes <op-dev@bit-1.de>
2013-07-11 18:17:00 +0200

    Added vertical velocity as a control loop input to improve dynamics during commanded ascend/decent.

--- flight/modules/AltitudeHold/altitudehold.c
+++ flight/modules/AltitudeHold/altitudehold.c
@@ -108,8 +108,8 @@ int32_t AltitudeHoldInitialize()
 MODULE_INITCALL(AltitudeHoldInitialize, AltitudeHoldStart);
 
 float tau;
-float throttleIntegral;
-float velocity;
+float altitudeIntegral;
+float velocityIntegral;
 float decay;
 float velocity_decay;
 bool running = false;
@@ -126,7 +126,7 @@ static void altitudeHoldTask(__attribute__((unused)) void *parameters)
     AltitudeHoldDesiredData altitudeHoldDesired;
     StabilizationDesiredData stabilizationDesired;
     AltHoldSmoothedData altHold;
-    float q[4], Rbe[3][3];
+    float q[4], Rbe[3][3], fblimit = 0;
 
     portTickType thisTime, lastUpdateTime;
     UAVObjEvent ev;
@@ -157,7 +157,7 @@ static void altitudeHoldTask(__attribute__((unused)) void *parameters)
         // Wait until the AttitudeRaw object is updated, if a timeout then go to failsafe
         if (xQueueReceive(queue, &ev, 100 / portTICK_RATE_MS) != pdTRUE) {
             if (!running) {
-                throttleIntegral = 0;
+                altitudeIntegral = 0;
             }
 
             // Todo: Add alarm if it should be running
@@ -171,17 +171,19 @@ static void altitudeHoldTask(__attribute__((unused)) void *parameters)
             altitudeHoldFlightMode = flightMode == FLIGHTSTATUS_FLIGHTMODE_ALTITUDEHOLD || flightMode == FLIGHTSTATUS_FLIGHTMODE_ALTITUDEVARIO;
             if (altitudeHoldFlightMode && !running) {
                 // Copy the current throttle as a starting point for integral
-                StabilizationDesiredThrottleGet(&throttleIntegral);
-                switchThrottle = throttleIntegral;
-                throttleIntegral *= Rbe[2][2]; // rotate into earth frame
-                if (throttleIntegral > 1) {
-                    throttleIntegral = 1;
-                } else if (throttleIntegral < 0) {
-                    throttleIntegral = 0;
+                float initThrottle;
+                StabilizationDesiredThrottleGet(&initThrottle);
+                initThrottle *= Rbe[2][2]; // rotate into earth frame
+                if (initThrottle > 1) {
+                    initThrottle = 1;
+                } else if (initThrottle < 0) {
+                    initThrottle = 0;
                 }
                 error    = 0;
-                velocity = 0;
+                altitudeHoldDesired.Velocity = 0;
                 altitudeHoldDesired.Altitude = altHold.Altitude;
+                altitudeIntegral = altHold.Altitude * altitudeHoldSettings.Kp + initThrottle;
+                velocityIntegral = 0;
                 running  = true;
             } else if (!altitudeHoldFlightMode) {
                 running = false;
@@ -362,11 +364,9 @@ static void altitudeHoldTask(__attribute__((unused)) void *parameters)
                 continue;
             }
 
-            // Compute the altitude error
-            error = altitudeHoldDesired.Altitude - altHold.Altitude;
-
-            // Compute integral off altitude error
-            throttleIntegral += error * altitudeHoldSettings.Ki * dT;
+            // Compute altitude and velocity integral
+            altitudeIntegral += (altitudeHoldDesired.Altitude - altHold.Altitude - fblimit) * altitudeHoldSettings.AltitudeKi * dT;
+            velocityIntegral += (altitudeHoldDesired.Velocity - altHold.Velocity - fblimit) * altitudeHoldSettings.VelocityKi * dT;
 
             // Only update stabilizationDesired less frequently
             if ((thisTime - lastUpdateTime) < 20) {
@@ -378,17 +378,19 @@ static void altitudeHoldTask(__attribute__((unused)) void *parameters)
             // Instead of explicit limit on integral you output limit feedback
             StabilizationDesiredGet(&stabilizationDesired);
             if (!enterFailSafe) {
-                stabilizationDesired.Throttle = error * altitudeHoldSettings.Kp + throttleIntegral -
-                                                altHold.Velocity * altitudeHoldSettings.Kd - altHold.Accel * altitudeHoldSettings.Ka;
+                stabilizationDesired.Throttle = altitudeIntegral + velocityIntegral
+                                                - altHold.Altitude * altitudeHoldSettings.Kp
+                                                - altHold.Velocity * altitudeHoldSettings.Kd
+                                                - altHold.Accel * altitudeHoldSettings.Ka;
                 // scale up throttle to compensate for roll/pitch angle but limit this to 60 deg (cos(60) == 0.5) to prevent excessive scaling
                 float throttlescale = Rbe[2][2] < 0.5f ? 0.5f : Rbe[2][2];
                 stabilizationDesired.Throttle /= throttlescale;
-
+                fblimit = 0;
                 if (stabilizationDesired.Throttle > 1) {
-                    throttleIntegral -= (stabilizationDesired.Throttle - 1);
+                    fblimit = stabilizationDesired.Throttle - 1;
                     stabilizationDesired.Throttle = 1;
                 } else if (stabilizationDesired.Throttle < 0) {
-                    throttleIntegral -= stabilizationDesired.Throttle;
+                    fblimit = stabilizationDesired.Throttle;
                     stabilizationDesired.Throttle = 0;
                 }
             } else {
--- flight/modules/ManualControl/manualcontrol.c
+++ flight/modules/ManualControl/manualcontrol.c
@@ -860,12 +860,15 @@ static void altitudeHoldDesired(ManualControlCommandData *cmd, bool changed)
     } else if (cmd->Throttle > DEADBAND_HIGH && zeroed) {
         // being the two band symmetrical I can divide by DEADBAND_LOW to scale it to a value betweeon 0 and 1
         // then apply an "exp" f(x,k) = (k*x*x*x + (255-k)*x) / 255
-        altitudeHoldDesiredData.Altitude += (throttleExp * powf((cmd->Throttle - DEADBAND_HIGH) / (DEADBAND_LOW), 3) + (255 - throttleExp) * (cmd->Throttle - DEADBAND_HIGH) / DEADBAND_LOW) / 255 * throttleRate * dT;
+        altitudeHoldDesiredData.Velocity = (throttleExp * powf((cmd->Throttle - DEADBAND_HIGH) / (DEADBAND_LOW), 3) + (255 - throttleExp) * (cmd->Throttle - DEADBAND_HIGH) / DEADBAND_LOW) / 255 * throttleRate;
+        altitudeHoldDesiredData.Altitude += altitudeHoldDesiredData.Velocity * dT;
     } else if (cmd->Throttle < DEADBAND_LOW && zeroed) {
-        altitudeHoldDesiredData.Altitude -= (throttleExp * powf((DEADBAND_LOW - (cmd->Throttle < 0 ? 0 : cmd->Throttle)) / DEADBAND_LOW, 3) + (255 - throttleExp) * (DEADBAND_LOW - cmd->Throttle) / DEADBAND_LOW) / 255 * throttleRate * dT;
+        altitudeHoldDesiredData.Velocity = -(throttleExp * powf((DEADBAND_LOW - (cmd->Throttle < 0 ? 0 : cmd->Throttle)) / DEADBAND_LOW, 3) + (255 - throttleExp) * (DEADBAND_LOW - cmd->Throttle) / DEADBAND_LOW) / 255 * throttleRate;
+        altitudeHoldDesiredData.Altitude += altitudeHoldDesiredData.Velocity * dT;
     } else if (cmd->Throttle >= DEADBAND_LOW && cmd->Throttle <= DEADBAND_HIGH && (throttleRate != 0)) {
         // Require the stick to enter the dead band before they can move height
         // Vario is not "engaged" when throttleRate == 0
+        altitudeHoldDesiredData.Velocity = 0;
         zeroed = true;
     }
 

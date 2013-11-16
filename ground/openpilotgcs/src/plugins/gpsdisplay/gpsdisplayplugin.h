58109f84611710e2bc2cce05efa875d24b2a3837
Corvus Corax <corvuscorax@cybertrench.com>
2013-11-16 17:34:40 +0100

    uncrustification for ground/openpilotgcs/src/plugins/gpsdisplay/gpsdisplayplugin.h

--- ground/openpilotgcs/src/plugins/gpsdisplay/gpsdisplayplugin.h
+++ ground/openpilotgcs/src/plugins/gpsdisplay/gpsdisplayplugin.h
@@ -34,7 +34,7 @@ class GpsDisplayGadgetFactory;
 
 class GpsDisplayPlugin : public ExtensionSystem::IPlugin {
     Q_OBJECT
-    Q_PLUGIN_METADATA(IID "OpenPilot.GpsDisplay")
+                                                 Q_PLUGIN_METADATA(IID "OpenPilot.GpsDisplay")
 
 public:
     GpsDisplayPlugin();

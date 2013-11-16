2e5c179fa43e0a02b4bdfa92a552112d11c4ecd6
Corvus Corax <corvuscorax@cybertrench.com>
2013-11-16 17:34:40 +0100

    uncrustification for ground/openpilotgcs/src/plugins/debuggadget/debugplugin.h

--- ground/openpilotgcs/src/plugins/debuggadget/debugplugin.h
+++ ground/openpilotgcs/src/plugins/debuggadget/debugplugin.h
@@ -34,7 +34,7 @@ class DebugGadgetFactory;
 
 class DebugPlugin : public ExtensionSystem::IPlugin {
     Q_OBJECT
-    Q_PLUGIN_METADATA(IID "OpenPilot.Debug")
+                                            Q_PLUGIN_METADATA(IID "OpenPilot.Debug")
 
 public:
     DebugPlugin();

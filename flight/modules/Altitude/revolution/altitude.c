44269b67623069b576294eb8c545227bd56f7e09
Alessio Morale <alessiomorale@gmail.com>
2013-12-10 02:02:30 +0100

    OP-1139: export corrected pressure value to uavobject

--- flight/modules/Altitude/revolution/altitude.c
+++ flight/modules/Altitude/revolution/altitude.c
@@ -164,9 +164,9 @@ static void altitudeTask(__attribute__((unused)) void *parameters)
         temp  = PIOS_MS5611_GetTemperature();
         press = PIOS_MS5611_GetPressure();
         float temp2      = temp * temp;
-        float correction = baroCorrection.a + temp * baroCorrection.b + temp2 * baroCorrection.c + temp * temp2 * baroCorrection.d;
+        press = press - baroCorrection.a + temp * baroCorrection.b + temp2 * baroCorrection.c + temp * temp2 * baroCorrection.d;
 
-        float altitude   = 44330.0f * (1.0f - powf((press - correction) / MS5611_P0, (1.0f / 5.255f)));
+        float altitude   = 44330.0f * (1.0f - powf((press) / MS5611_P0, (1.0f / 5.255f)));
 
         if (!isnan(altitude)) {
             data.Altitude    = altitude;

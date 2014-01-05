6b3a02c485cf200624d04dbc6ce5ada330c42f9a
Philippe Renon <op.filnet@yahoo.fr>
2014-01-05 18:45:57 +0100

    [OP-1105] improved handling of missing firmware description in GCS

--- ground/openpilotgcs/src/plugins/uavobjectutil/uavobjectutilmanager.cpp
+++ ground/openpilotgcs/src/plugins/uavobjectutil/uavobjectutilmanager.cpp
@@ -301,6 +301,13 @@ QByteArray UAVObjectUtilManager::getBoardDescription()
     return ret;
 }
 
+QString UAVObjectUtilManager::getBoardDescriptionString()
+{
+    QByteArray arr = getBoardDescription();
+
+    int index = arr.indexOf(255);
+    return QString((index == -1) ? arr : arr.left(index));
+}
 
 // ******************************
 // HomeLocation
@@ -474,5 +481,3 @@ bool UAVObjectUtilManager::descriptionToStructure(QByteArray desc, deviceDescrip
     }
     return false;
 }
-
-// ******************************

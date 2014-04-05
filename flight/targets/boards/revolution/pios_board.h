bb91039f51dce53a72c35b5f5db28c6232e905a1
Corvus Corax <corvuscorax@cybertrench.com>
2014-04-05 12:53:31 +0200

    OP-1290 fixes in telemetry (and pios_board initialization)

--- flight/targets/boards/revolution/pios_board.h
+++ flight/targets/boards/revolution/pios_board.h
@@ -141,6 +141,7 @@ extern uint32_t pios_i2c_flexiport_adapter_id;
 // -------------------------
 #define PIOS_COM_MAX_DEVS 4
 extern uint32_t pios_com_telem_rf_id;
+extern uint32_t pios_com_rf_id;
 extern uint32_t pios_com_gps_id;
 extern uint32_t pios_com_telem_usb_id;
 extern uint32_t pios_com_bridge_id;
@@ -149,6 +150,7 @@ extern uint32_t pios_com_hkosd_id;
 #define PIOS_COM_GPS       (pios_com_gps_id)
 #define PIOS_COM_TELEM_USB (pios_com_telem_usb_id)
 #define PIOS_COM_TELEM_RF  (pios_com_telem_rf_id)
+#define PIOS_COM_RF        (pios_com_rf_id)
 #define PIOS_COM_BRIDGE    (pios_com_bridge_id)
 #define PIOS_COM_VCP       (pios_com_vcp_id)
 #define PIOS_COM_OSDHK     (pios_com_hkosd_id)

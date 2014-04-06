3018e73e9118dc960da316e4aa050ce201675242
Corvus Corax <corvuscorax@cybertrench.com>
2014-04-06 12:24:42 +0200

    OP-1290 saved some RAM by removing last alarm update time from UAVObject
    (static allocated array instead, and smaller element size)

--- flight/libraries/alarms.c
+++ flight/libraries/alarms.c
@@ -37,6 +37,7 @@
 
 // Private variables
 static xSemaphoreHandle lock;
+static volatile uint16_t lastAlarmChange[SYSTEMALARMS_ALARM_NUMELEM] = { 0 }; // this deliberately overflows every 2^16 milliseconds to save memory
 
 // Private functions
 static int32_t hasSeverity(SystemAlarmsAlarmOptions severity);
@@ -75,12 +76,12 @@ int32_t AlarmsSet(SystemAlarmsAlarmElem alarm, SystemAlarmsAlarmOptions severity
 
     // Read alarm and update its severity only if it was changed
     SystemAlarmsGet(&alarms);
-    uint32_t flightTime = xTaskGetTickCount() * portTICK_RATE_MS;
-    if ((flightTime - cast_struct_to_array(alarms.LastChange, alarms.LastChange.Actuator)[alarm] > 1000 &&
+    uint16_t flightTime = xTaskGetTickCount() * portTICK_RATE_MS; // this deliberately overflows every 2^16 milliseconds to save memory
+    if ((flightTime - lastAlarmChange[alarm] > 1000 &&
          cast_struct_to_array(alarms.Alarm, alarms.Alarm.Actuator)[alarm] != severity)
         || cast_struct_to_array(alarms.Alarm, alarms.Alarm.Actuator)[alarm] < severity) {
         cast_struct_to_array(alarms.Alarm, alarms.Alarm.Actuator)[alarm] = severity;
-        cast_struct_to_array(alarms.LastChange, alarms.LastChange.Actuator)[alarm] = flightTime;
+        lastAlarmChange[alarm] = flightTime;
         SystemAlarmsSet(&alarms);
     }
 
@@ -114,14 +115,14 @@ int32_t ExtendedAlarmsSet(SystemAlarmsAlarmElem alarm,
 
     // Read alarm and update its severity only if it was changed
     SystemAlarmsGet(&alarms);
-    uint32_t flightTime = xTaskGetTickCount() * portTICK_RATE_MS;
-    if ((flightTime - cast_struct_to_array(alarms.LastChange, alarms.LastChange.Actuator)[alarm] > 1000 &&
+    uint16_t flightTime = xTaskGetTickCount() * portTICK_RATE_MS; // this deliberately overflows every 2^16 milliseconds to save memory
+    if ((flightTime - lastAlarmChange[alarm] > 1000 &&
          cast_struct_to_array(alarms.Alarm, alarms.Alarm.Actuator)[alarm] != severity)
         || cast_struct_to_array(alarms.Alarm, alarms.Alarm.Actuator)[alarm] < severity) {
         cast_struct_to_array(alarms.ExtendedAlarmStatus, alarms.ExtendedAlarmStatus.BootFault)[alarm]    = status;
         cast_struct_to_array(alarms.ExtendedAlarmSubStatus, alarms.ExtendedAlarmStatus.BootFault)[alarm] = subStatus;
         cast_struct_to_array(alarms.Alarm, alarms.Alarm.Actuator)[alarm] = severity;
-        cast_struct_to_array(alarms.LastChange, alarms.LastChange.Actuator)[alarm] = flightTime;
+        lastAlarmChange[alarm] = flightTime;
         SystemAlarmsSet(&alarms);
     }
 

520e721bdd832b55838913a6550b5ee1f99141ff
Alessio Morale <alessiomorale@gmail.com>
2014-06-06 22:15:18 +0200

    OP-1365 Flight code instrumentation API: PiOS Implementation

--- make/apps-defs.mk
+++ make/apps-defs.mk
@@ -100,6 +100,7 @@ SRC += $(PIOSCOMMON)/pios_usb_util.c
 SRC += $(PIOSCOMMON)/pios_task_monitor.c
 SRC += $(PIOSCOMMON)/pios_callbackscheduler.c
 SRC += $(PIOSCOMMON)/pios_notify.c
+SRC += $(PIOSCOMMON)/pios_instrumentation.c
 ## Misc library functions
 SRC += $(FLIGHTLIB)/fifo_buffer.c
 SRC += $(FLIGHTLIB)/sanitycheck.c

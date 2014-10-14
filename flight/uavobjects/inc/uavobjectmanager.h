de68e0179fdfbb343abc3ce87f1a88ccef08710d
abeck70 <abeck70@icloud.com>
2014-10-14 17:17:59 +1100

    First round review comments

--- flight/uavobjects/inc/uavobjectmanager.h
+++ flight/uavobjects/inc/uavobjectmanager.h
@@ -202,10 +202,6 @@ uint8_t UAVObjUpdateCRC(UAVObjHandle obj_handle, uint16_t instId, uint8_t crc);
 int32_t UAVObjSave(UAVObjHandle obj_handle, uint16_t instId);
 int32_t UAVObjLoad(UAVObjHandle obj_handle, uint16_t instId);
 int32_t UAVObjDelete(UAVObjHandle obj_handle, uint16_t instId);
-#if defined(PIOS_INCLUDE_SDCARD)
-int32_t UAVObjSaveToFile(UAVObjHandle obj_handle, uint16_t instId);
-int32_t UAVObjLoadFromFile(UAVObjHandle obj_handle);
-#endif
 int32_t UAVObjSaveSettings();
 int32_t UAVObjLoadSettings();
 int32_t UAVObjDeleteSettings();

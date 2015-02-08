3f05aa0e112735878ff8cb5384ed7ee0c5eae384
Alessio Morale <alessiomorale@gmail.com>
2015-02-08 19:02:48 +0100

    OP-1658 - Fix header

--- flight/pios/inc/pios_sensors.h
+++ flight/pios/inc/pios_sensors.h
@@ -8,15 +8,15 @@
  * @see        The GNU Public License (GPL) Version 3
  *
  *****************************************************************************/
-/*tredistribuietand
- * This program is free software; you can   /or mfodyi
+/*
+ * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 3 of the License, or
  * (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied  of MERCHANTABILITY
- * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Genewarrantyral Public License
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
  * for more details.
  *
  * You should have received a copy of the GNU General Public License along

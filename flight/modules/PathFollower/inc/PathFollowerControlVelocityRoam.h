03edb049f06bf60054dce36a5b8b301a397284c4
abeck70 <abeck70@icloud.com>
2015-02-18 12:19:17 +1100

    OP-1696 Start of refactoring of VelocityRoam implementation in PathFollower
    Also TODOs:
    1. To support pathplanner, remove all references to FlightStatus object and add more fields to PathDesired
    2. Refactor Braking to use C++
    3. Test and optimise Velocity Roam
    4. Add more config options to Landing. For example disable disarming. And add more setting objects.
    5. Consider moving all landing settings to one object.

---
+++ flight/modules/PathFollower/inc/PathFollowerControlVelocityRoam.h
@@ -0,0 +1,82 @@
+/**
+ ******************************************************************************
+ * @addtogroup OpenPilotModules OpenPilot Modules
+ * @{
+ * @addtogroup PathFollower CONTROL interface class
+ * @brief CONTROL interface class for pathfollower goal fsm implementations
+ * @{
+ *
+ * @file       PathFollowerCONTROL.h
+ * @author     The OpenPilot Team, http://www.openpilot.org Copyright (C) 2015.
+ * @brief      Executes CONTROL for landing sequence
+ *
+ * @see        The GNU Public License (GPL) Version 3
+ *
+ *****************************************************************************/
+/*
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ */
+#ifndef PATHFOLLOWERCONTROLVELOCITYROAM_H
+#define PATHFOLLOWERCONTROLVELOCITYROAM_H
+#include "PathFollowerControl.h"
+#include "PIDControlThrust.h"
+#include "PIDControlNE.h"
+// forward decl
+class PathFollowerFSM;
+class PathFollowerControlVelocityRoam : PathFollowerControl {
+private:
+    static PathFollowerControlVelocityRoam *p_inst;
+    PathFollowerControlVelocityRoam();
+
+
+public:
+    static PathFollowerControlVelocityRoam *instance()
+    {
+        if (!p_inst) {
+            p_inst = new PathFollowerControlVelocityRoam();
+        }
+        return p_inst;
+    }
+
+    int32_t Initialize(PathFollowerFSM *fsm_ptr,
+                       VtolPathFollowerSettingsData *vtolPathFollowerSettings,
+                       PathDesiredData *pathDesired,
+                       FlightStatusData *flightStatus,
+                       PathStatusData *pathStatus);
+
+
+    void Activate(void);
+    void Deactivate(void);
+    void SettingsUpdated(void);
+    void UpdateAutoPilot(void);
+    void ObjectiveUpdated(void);
+    uint8_t IsActive(void);
+    uint8_t Mode(void);
+
+private:
+    void UpdateVelocityDesired(void);
+    int8_t UpdateStabilizationDesired(bool yaw_attitude, float yaw_direction);
+
+    // PathFollowerFSM *fsm;
+    VtolPathFollowerSettingsData *vtolPathFollowerSettings;
+    PathDesiredData *pathDesired;
+    FlightStatusData *flightStatus;
+    PathStatusData *pathStatus;
+    PIDControlThrust controlThrust;
+    PIDControlNE controlNE;
+    uint8_t mActive;
+};
+
+#endif // PATHFOLLOWERCONTROLVELOCITYROAM_H

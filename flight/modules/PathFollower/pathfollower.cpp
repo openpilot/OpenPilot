100cf335d187fe696d3678371b194fc3044a1c7e
Alex Beck <abeck70@icloud.com>
2015-03-05 22:32:30 +1100

    OP-1696 simposix c++ compilation fix

--- flight/modules/PathFollower/pathfollower.cpp
+++ flight/modules/PathFollower/pathfollower.cpp
@@ -128,7 +128,7 @@ static void pathFollowerInitializeControllersForFrameType();
  * Initialise the module, called on startup
  * \returns 0 on success or -1 if initialisation failed
  */
-extern "C" uint32_t PathFollowerStart()
+extern "C" int32_t PathFollowerStart()
 {
     // Start main task
     PathStatusGet(&pathStatus);
@@ -143,7 +143,7 @@ extern "C" uint32_t PathFollowerStart()
  * Initialise the module, called on startup
  * \returns 0 on success or -1 if initialisation failed
  */
-extern "C" uint32_t PathFollowerInitialize()
+extern "C" int32_t PathFollowerInitialize()
 {
     // initialize objects
     GroundPathFollowerSettingsInitialize();
@@ -191,9 +191,7 @@ extern "C" uint32_t PathFollowerInitialize()
 
     return 0;
 }
-extern "C" {
 MODULE_INITCALL(PathFollowerInitialize, PathFollowerStart);
-}
 
 void pathFollowerInitializeControllersForFrameType()
 {

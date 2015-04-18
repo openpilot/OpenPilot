10c537352351aaf432aa5e2d8d5b106ad7672a74
Mike Carr <mcarr67@gmail.com>
2015-04-18 14:16:22 -0700

    cleanup not needed items, based on code review suggestions

--- make/vagrant/vagrant_openpilot_dev/bootstrap.sh
+++ make/vagrant/vagrant_openpilot_dev/bootstrap.sh
@@ -4,7 +4,7 @@ apt-get update
 apt-get -y upgrade
 
 # get ourselves some basic toys
-apt-get -y install git-core build-essential openssl libssl-dev subversion
+apt-get -y install git-core build-essential openssl libssl-dev 
 
 # install some tools for the vagrant user only by executing
 # a script AS the vagrant user
@@ -14,6 +14,3 @@ chgrp vagrant /home/vagrant/install-tools.sh
 chmod ug+x /home/vagrant/install-tools.sh
 su -c "/home/vagrant/install-tools.sh" vagrant
 
-# set up udev rules for known android device manufacturers
-cat /vagrant/51-android.rules > /etc/udev/rules.d/51-android.rules
-chmod a+r /etc/udev/rules.d/51-android.rules

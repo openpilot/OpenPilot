04b2731c36b36339b29e4f14b93dde266054a22d
Laurent Lalanne <f5soh@free.fr>
2015-04-20 12:57:18 +0200

    OP-1822 Fix : Add one byte, allow last helitype 2servo/90° (return to custom bug)

--- ground/openpilotgcs/src/plugins/config/cfg_vehicletypes/vehicleconfig.h
+++ ground/openpilotgcs/src/plugins/config/cfg_vehicletypes/vehicleconfig.h
@@ -60,7 +60,7 @@ typedef struct {
 } __attribute__((packed))  multiGUISettingsStruct;
 
 typedef struct {
-    uint    SwashplateType : 3;
+    uint    SwashplateType : 4;
     uint    FirstServoIndex : 2;
     uint    CorrectionAngle : 9;
     uint    ccpmCollectivePassthroughState : 1;
@@ -75,7 +75,7 @@ typedef struct {
     uint    ServoIndexZ : 4; // 57 bits
     uint    Throttle : 4;
     uint    Tail : 4; // 65bits
-    quint32 padding : 31; // 96 bits
+    quint32 padding : 30; // 96 bits
     quint32 padding1; // 128 bits
 } __attribute__((packed))  heliGUISettingsStruct;
 

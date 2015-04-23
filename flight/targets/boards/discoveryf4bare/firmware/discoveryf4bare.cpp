0d77cd95d91cafd2bed604f1cc7b6649194c2780
abeck70 <abeck70@icloud.com>
2015-04-23 21:13:00 +1000

    OP-1848 altvari0
    1. build fixes
    2. Updated ui to use updated uavo names in altitude settings that align to the naming in vtol settings
    3. Moved call the thrust altvario to pre application of PIDs for 3d axes to allow for later coupling.

--- flight/targets/boards/discoveryf4bare/firmware/discoveryf4bare.cpp
+++ flight/targets/boards/discoveryf4bare/firmware/discoveryf4bare.cpp
@@ -31,7 +31,7 @@
  * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
  */
 
-
+extern "C" {
 #include "inc/openpilot.h"
 #include <uavobjectsinit.h>
 
@@ -74,6 +74,7 @@ static void initTask(void *parameters);
 
 /* Prototype of generated InitModules() function */
 extern void InitModules(void);
+}
 
 /**
  * OpenPilot Main function:

6ee9eda7fb86cd5b28e481950db08c132caabe2f
Kevin Finisterre <kf@digitalmunition.com>
2015-04-28 15:31:27 -0400

    Fix the error "Err  Field '0' (actuatorcommand.Channel.0) is signed (FT_INT16) but is being displayed unsigned (BASE_DEC_HEX)" Stop attempting to display in hex

--- ground/uavobjgenerator/generators/wireshark/uavobjectgeneratorwireshark.cpp
+++ ground/uavobjgenerator/generators/wireshark/uavobjectgeneratorwireshark.cpp
@@ -233,7 +233,7 @@ bool UAVObjectGeneratorWireshark::process_object(ObjectInfo *info, QDir outputpa
             } else if (info->fields[n]->type == FIELDTYPE_FLOAT32) {
                 headerfields.append(QString("\t     BASE_NONE, NULL, 0x0, NULL, HFILL \r\n"));
             } else {
-                headerfields.append(QString("\t     BASE_DEC_HEX, NULL, 0x0, NULL, HFILL\r\n"));
+                headerfields.append(QString("\t     BASE_DEC, NULL, 0x0, NULL, HFILL\r\n"));
             }
             headerfields.append(QString("\t   },\r\n"));
             headerfields.append(QString("\t },\r\n"));
@@ -268,7 +268,7 @@ bool UAVObjectGeneratorWireshark::process_object(ObjectInfo *info, QDir outputpa
                 } else if (info->fields[n]->type == FIELDTYPE_FLOAT32) {
                     headerfields.append(QString("\t     BASE_NONE, NULL, 0x0, NULL, HFILL \r\n"));
                 } else {
-                    headerfields.append(QString("\t     BASE_DEC_HEX, NULL, 0x0, NULL, HFILL\r\n"));
+                    headerfields.append(QString("\t     BASE_DEC, NULL, 0x0, NULL, HFILL\r\n"));
                 }
                 headerfields.append(QString("\t   },\r\n"));
                 headerfields.append(QString("\t },\r\n"));
